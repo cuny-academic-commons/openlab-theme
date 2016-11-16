0f13438a9f307986c09e6006052df00226d1a3f5
Boone B Gorges <boonebgorges@gmail.com>
2016-11-16 12:32:34 -0600

    Don't use BP functions unless they exist.
    See #1.

--- lib/content-processing.php
+++ lib/content-processing.php
@@ -29,8 +29,8 @@ function openlab_conditional_body_classes( $classes ) {
 	$my_group_pages = array( 'my-courses', 'my-clubs', 'my-projects' );
 
 	if ( ( isset( $post->post_name ) && in_array( $post->post_name, $group_archives ) ) ||
-			bp_is_single_item() ||
-			bp_is_user() ||
+			( function_exists( 'bp_is_single_item' ) && bp_is_single_item() ) ||
+			( function_exists( 'bp_is_user' ) && bp_is_user() ) ||
 			( isset( $post->post_name ) && $post->ID == $about_page_obj->ID ) ||
 			( isset( $post->post_parent ) && $post->post_parent == $about_page_obj->ID ) ||
 			( isset( $post->post_parent ) && $post->post_parent == $calendar_page_obj->ID ) ||
