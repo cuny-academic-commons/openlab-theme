423be09f21a756f1476a26a3269771c84695454a
Boone B Gorges <boonebgorges@gmail.com>
2016-11-28 12:49:44 -0600

    Code formatting.

--- home.php
+++ home.php
@@ -9,55 +9,55 @@ get_header();
 ?>
 
 <div id="openlab-main-content" class="clearfix row-home-top">
-    <div class="no-gutter no-gutter-right login">
-        <div id="cuny_openlab_jump_start">
-            <?php cuny_home_login(); ?>
-        </div>
-    </div>
-    <div class="fill-gutter fill-gutter-left slider">
-        <?php echo openlab_get_home_slider(); ?>
-    </div>
+	<div class="no-gutter no-gutter-right login">
+		<div id="cuny_openlab_jump_start">
+			<?php cuny_home_login(); ?>
+		</div>
+	</div>
+	<div class="fill-gutter fill-gutter-left slider">
+		<?php echo openlab_get_home_slider(); ?>
+	</div>
 
 </div>
 <div class="row row-home-bottom">
-    <div id="home-left" class="col-sm-8">
-        <?php dynamic_sidebar('cac-featured') ?>
+	<div id="home-left" class="col-sm-8">
+		<?php dynamic_sidebar( 'cac-featured' ) ?>
 
-        <div class="box-1 left-box">
-            <h2 class="title uppercase clearfix"><i id="refreshActivity" class="fa fa-refresh pull-right" aria-hidden="true"></i>What's Happening On OpenLab?</h2>
-            <div id="whatsHappening" class="left-block-content whats-happening-wrapper">
+		<div class="box-1 left-box">
+			<h2 class="title uppercase clearfix"><i id="refreshActivity" class="fa fa-refresh pull-right" aria-hidden="true"></i>What's Happening On OpenLab?</h2>
+			<div id="whatsHappening" class="left-block-content whats-happening-wrapper">
 		<?php /* This will be a widget area
                 <?php echo openlab_whats_happening(); ?>
 		*/ ?>
-            </div>
-        </div>
+			</div>
+		</div>
 
-        <div class="box-1 left-box" id="whos-online">
-            <h2 class="title uppercase">Who's Online?</h2>
+		<div class="box-1 left-box" id="whos-online">
+			<h2 class="title uppercase">Who's Online?</h2>
 		<?php /* This will be a widget area
             <?php cuny_whos_online(); ?>
 	    */ ?>
-        </div>
+		</div>
 
 	<?php /* This will be a widget area
         <?php cuny_home_new_members(); ?>
 	*/ ?>
-    </div>
-    <div id="home-right" class="col-sm-16">
-        <div id="home-group-list-wrapper" class="row">
+	</div>
+	<div id="home-right" class="col-sm-16">
+		<div id="home-group-list-wrapper" class="row">
 	<?php /* These will be widget areas
             <?php cuny_home_square('course'); ?>
             <?php cuny_home_square('project'); ?>
             <?php cuny_home_square('club'); ?>
             <?php cuny_home_square('portfolio'); ?>
 	    */ ?>
-            <div class="clearfloat"></div>
-            <script type='text/javascript'>(function ($) {
-                    $('.activity-list').css('visibility', 'hidden');
-                    $('#home-new-member-wrap').css('visibility', 'hidden');
-                })(jQuery);</script>
-        </div>
-    </div>
+			<div class="clearfloat"></div>
+			<script type='text/javascript'>(function ($) {
+					$('.activity-list').css('visibility', 'hidden');
+					$('#home-new-member-wrap').css('visibility', 'hidden');
+				})(jQuery);</script>
+		</div>
+	</div>
 </div>
 
 <?php
