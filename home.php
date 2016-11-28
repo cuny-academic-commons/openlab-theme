75c2e01a673d0d61fd1c066c9fe3646a893f8e9b
Boone B Gorges <boonebgorges@gmail.com>
2016-11-28 12:46:40 -0600

    Code formatting.

--- home.php
+++ home.php
@@ -26,23 +26,31 @@ get_header();
         <div class="box-1 left-box">
             <h2 class="title uppercase clearfix"><i id="refreshActivity" class="fa fa-refresh pull-right" aria-hidden="true"></i>What's Happening On OpenLab?</h2>
             <div id="whatsHappening" class="left-block-content whats-happening-wrapper">
+		<?php /* This will be a widget area
                 <?php echo openlab_whats_happening(); ?>
+		*/ ?>
             </div>
         </div>
 
         <div class="box-1 left-box" id="whos-online">
             <h2 class="title uppercase">Who's Online?</h2>
+		<?php /* This will be a widget area
             <?php cuny_whos_online(); ?>
+	    */ ?>
         </div>
 
+	<?php /* This will be a widget area
         <?php cuny_home_new_members(); ?>
+	*/ ?>
     </div>
     <div id="home-right" class="col-sm-16">
         <div id="home-group-list-wrapper" class="row">
+	<?php /* These will be widget areas
             <?php cuny_home_square('course'); ?>
             <?php cuny_home_square('project'); ?>
             <?php cuny_home_square('club'); ?>
             <?php cuny_home_square('portfolio'); ?>
+	    */ ?>
             <div class="clearfloat"></div>
             <script type='text/javascript'>(function ($) {
                     $('.activity-list').css('visibility', 'hidden');
