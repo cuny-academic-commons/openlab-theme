ca709d7ba18e1479349bb0679f3f885681980c74
Boone B Gorges <boonebgorges@gmail.com>
2017-09-20 20:19:50 -0500

    Fix variable name typo causing error on forum topic creation.
    See #31.

--- lib/plugin-hooks.php
+++ lib/plugin-hooks.php
@@ -481,7 +481,7 @@ add_filter( 'bbp_is_forum_public', 'openlab_enforce_forum_privacy', 10, 2 );
  *
  * This can cause a costly tree rebuild. See bbPress #1799. See OL #1663,
  */
-function openlab_prevent_bbp_recounts( $args ) {
+function openlab_prevent_bbp_recounts( $r ) {
 	if ( bbp_get_group_forums_root_id() == $r['forum_id'] ) {
 		$r['forum_id'] = 0;
 	}
