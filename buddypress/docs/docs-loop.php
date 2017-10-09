2d300794c7414e5997fb295103097c5fa597ce0d
Boone B Gorges <boonebgorges@gmail.com>
2017-10-09 16:18:57 -0500

    Fix syntax error.

--- buddypress/docs/docs-loop.php
+++ buddypress/docs/docs-loop.php
@@ -110,7 +110,7 @@
 	</div>
 
 <?php else : ?>
-	<?php if ( groups_is_user_member( get_current_user_id(), bp_get_current_group_id()() ) ) :  ?>
+	<?php if ( groups_is_user_member( get_current_user_id(), bp_get_current_group_id() ) ) :  ?>
 		<p class="no-docs bold"><?php printf( __( 'There are no docs for this view. Why not <a href="%s">create one</a>?', 'openlab-theme' ), bp_docs_get_item_docs_link() . BP_DOCS_CREATE_SLUG ) ?></p>
 	<?php else : ?>
 		<p class="no-docs bold"><?php printf( __( 'There are no docs for this view.', 'openlab-theme' ) ) ?></p>
