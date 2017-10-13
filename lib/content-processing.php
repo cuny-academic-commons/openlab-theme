7d8e74ab6718c94361caa1103f6f2d7ea02af43c
Boone B Gorges <boonebgorges@gmail.com>
2017-10-13 20:30:17 +0000

    Un-hardcode calendar page check.
    This depends on the integration of calendar pages into Brand Settings.
    
    See #3, #76.

--- lib/content-processing.php
+++ lib/content-processing.php
@@ -29,13 +29,16 @@ function openlab_conditional_body_classes( $classes ) {
 	}
 
 	$calendar_page_obj = get_page_by_path( 'about/calendar' );
-	$my_group_pages = array( 'my-courses', 'my-clubs', 'my-projects' );
+	$is_calendar_page = cboxol_is_brand_page( 'calendar' );
+	if ( ! $is_calendar_page && ! empty( $post->post_parent ) ) {
+		$is_calendar_page = cboxol_is_brand_page( 'calendar', $post->post_parent );
+	}
 
 	if ( ( bp_is_groups_directory() || bp_is_members_directory() ) ||
 			( function_exists( 'bp_is_single_item' ) && bp_is_single_item() ) ||
 			( function_exists( 'bp_is_user' ) && bp_is_user() ) ||
 			( $is_about_page ) ||
-			( isset( $post->post_parent ) && $calendar_page_obj && $post->post_parent == $calendar_page_obj->ID ) ||
+			( $is_calendar_page ) ||
 			( isset( $post->post_type ) && $post->post_type == 'help' ) ||
 			( isset( $post->post_type ) && $post->post_type == 'help_glossary' ) ||
 			( ! empty( $query_vars ) && isset( $query_vars['help_category'] ) ) ) {
