51bf05cce1bb8af1d692bfaaf0376ac40a31e929
Boone B Gorges <boonebgorges@gmail.com>
2017-10-17 20:08:53 +0000

    Fix link generation for OL-wide calendar iCal page.
    See #73.

--- parts/source/pages/openlab-calendar.php
+++ parts/source/pages/openlab-calendar.php
@@ -1,21 +1,21 @@
 <div class="action-events">
 	<div id="item-body">
         <div class="submenu submenu-sitewide-calendar">
-            <div class="submenu-text pull-left bold">Calendar:</div>
+            <div class="submenu-text pull-left bold"><?php esc_html_e( 'Calendar:', 'openlab-theme' ); ?></div>
             <ul class="nav nav-inline">
-                <?php foreach ($menu_items as $item): ?>
-                    <li id="<?php echo $item['slug'] ?>-groups-li" class="<?php echo $item['class'] ?>"><a href="<?php echo $item['link'] ?>"><?php echo $item['name'] ?></a></li>
+                <?php foreach ( $menu_items as $item ): ?>
+                    <li id="<?php echo esc_attr( $item['slug'] ); ?>-groups-li" class="<?php echo esc_attr( $item['class'] ); ?>"><a href="<?php echo esc_url( $item['link'] ); ?>"><?php echo esc_html( $item['name'] ); ?></a></li>
                 <?php endforeach; ?>
             </ul>
         </div>
-		
+
 		<div id="openlabCalendar" class="calendar-wrapper">
-		    <?php echo eo_get_event_fullcalendar($args); ?>
+		    <?php echo eo_get_event_fullcalendar( $args ); ?>
 		</div>
-		
+
 		<div id="bpeo-ical-download">
-		    <h3><?php echo __('Subscribe', 'bp-event-organiser'); ?></h3>
-		    <li><a class="bpeo-ical-link" href="{$link}"><span class="icon"></span><?php echo __('Download iCalendar file (Public)', 'bp-event-organiser'); ?></a></li>
+		    <h3><?php esc_html_e( 'Subscribe', 'openlab-theme' ); ?></h3>
+		    <li><a class="bpeo-ical-link" href="<?php echo esc_url( $link ); ?>"><span class="icon"></span><?php esc_html_e( 'Download iCalendar file (Public)', 'openlab-theme' ); ?></a></li>
 		</div>
 	</div>
 </div>
