9464cb0bf20c5cd48bcfeda245152e224de8a024
Boone B Gorges <boonebgorges@gmail.com>
2017-10-23 18:27:56 -0500

    i18n and remove unused stuff on Update Settings panel.

--- buddypress/members/single/settings/general.php
+++ buddypress/members/single/settings/general.php
@@ -16,14 +16,14 @@ do_action( 'bp_before_member_settings_template' );
 	<form action="<?php echo bp_displayed_user_domain() . bp_get_settings_slug() . '/general'; ?>" method="post" class="standard-form form-panel" id="settings-form">
 
 		<div class="panel panel-default">
-			<div class="panel-heading">Update Settings</div>
+			<div class="panel-heading"><?php esc_html_e( 'Update Settings', 'openlab-theme' ); ?></div>
 				<div class="panel-body">
 	            	<?php do_action( 'template_notices' ); ?>
 
 		<div class="form-group settings-section username-section">
 			<label for="username">Username</label>
 			<input class="form-control" type="text" id="username" disabled="disabled" value="<?php bp_displayed_user_username() ?>" />
-			<p class="description">Your username cannot be changed.</p>
+			<p class="description"><?php esc_html_e( 'Your username cannot be changed.', 'openlab-theme' ); ?></p>
 		</div>
 
 		<div class="form-group settings-section email-section">
@@ -32,7 +32,7 @@ do_action( 'bp_before_member_settings_template' );
 		</div>
 
 		<div class="form-group settings-section current-pw-section">
-			<label for="pwd">Current Password</label>
+			<label for="pwd"><?php esc_html_e( 'Current Password', 'openlab-theme' ); ?></label>
 			<input class="form-control" type="password" name="pwd" id="pwd" size="16" value="" class="settings-input small" />
 
 			<?php
@@ -40,43 +40,25 @@ do_action( 'bp_before_member_settings_template' );
 			$include_acct_type = in_array( $account_type, array( 'Student', 'Alumni' ) ) ? ' account type, ' : ' ';
 			?>
 
-			<p class="description">Required to change<?php echo $include_acct_type ?>current password, first name, or last name. <a class="underline" href="<?php echo site_url( add_query_arg( array( 'action' => 'lostpassword' ), 'wp-login.php' ), 'login' ); ?>" title="<?php _e( 'Password Lost and Found', 'buddypress' ); ?>"><?php _e( 'Lost your password?', 'buddypress' ); ?></a></p>
+			<p class="description"><?php esc_html_e( 'Required to change email address or password.', 'openlab-theme' ); ?> <a class="underline" href="<?php echo site_url( add_query_arg( array( 'action' => 'lostpassword' ), 'wp-login.php' ), 'login' ); ?>" title="<?php _e( 'Password Lost and Found', 'openlab-theme' ); ?>"><?php _e( 'Lost your password?', 'openlab-theme' ); ?></a></p>
 		</div>
 
 		<div class="form-group settings-section change-pw-section">
-			<label for="pass1">Change Password</label>
+			<label for="pass1"><?php esc_html_e( 'Change Password', 'openlab-theme' ); ?></label>
 			<input class="form-control" type="password" name="pass1" id="pass1" size="16" value="" class="settings-input small" />
 
-			<label for="pass1">Confirm Change Password</label>
+			<label for="pass1"><?php esc_html_e( 'Confirm Change Password', 'openlab-theme' ); ?></label>
 			<input class="form-control" type="password" name="pass2" id="pass2" size="16" value="" class="settings-input small" />
 
-			<p class="description">Leave blank for no change</p>
+			<p class="description"><?php esc_html_e( 'Leave blank for no change.', 'openlab-theme' ); ?></p>
 		</div>
-
-		<div class="form-group settings-section name-section">
-			<label for="fname">First Name (required)</label>
-			<input class="form-control" type="text" name="fname" id="fname" value="<?php echo bp_get_profile_field_data( array( 'field' => 'First Name' ) ) ?>" />
-
-			<label for="lname">Last Name (required)</label>
-			<input class="form-control" type="text" name="lname" id="lname" value="<?php echo bp_get_profile_field_data( array( 'field' => 'Last Name' ) ) ?>" />
-		</div>
-
-		<?php if ( in_array( $account_type, array( 'Student', 'Alumni' ) ) ) : ?>
-			<div class="form-group settings-section account-type-section">
-				<label for="account_type">Account Type</label>
-				<select class="form-control" name="account_type">
-					<option value="Student" <?php selected( 'Student', $account_type ) ?>>Student</option>
-					<option value="Alumni" <?php selected( 'Alumni', $account_type ) ?>>Alumni</option>
-				</select>
-			</div>
-		<?php endif ?>
 
 			</div>
 		</div>
 		<?php do_action( 'bp_core_general_settings_before_submit' ); ?>
 
 		<div class="submit">
-			<input class="btn btn-primary btn-margin btn-margin-top" type="submit" name="submit" value="<?php _e( 'Save Changes', 'buddypress' ); ?>" id="submit" class="auto" />
+			<input class="btn btn-primary btn-margin btn-margin-top" type="submit" name="submit" value="<?php esc_html_e( 'Save Changes', 'openlab-theme' ); ?>" id="submit" class="auto" />
 		</div>
 
 		<?php do_action( 'bp_core_general_settings_after_submit' );
