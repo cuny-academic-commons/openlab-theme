2662aa8d8a2ab47c369b6e3695c0294d60f3ac91
Boone B Gorges <boonebgorges@gmail.com>
2017-10-27 14:26:04 +0000

    i18n for request membership template.

--- buddypress/groups/single/request-membership.php
+++ buddypress/groups/single/request-membership.php
@@ -6,12 +6,12 @@
 			<p><?php echo esc_html( sprintf( __( "You are requesting to become a member of '%s'.", 'openlab-theme' ), bp_get_group_name( false ) ) ); ?></p>
 
 			<form action="<?php bp_group_form_action( 'request-membership' ); ?>" method="post" name="request-membership-form" id="request-membership-form" class="standard-form">
-				<label for="group-request-membership-comments"><?php _e( 'Comments (optional)', 'buddypress' ); ?></label>
+				<label for="group-request-membership-comments"><?php esc_html_e( 'Comments (optional)', 'openlab-theme' ); ?></label>
 				<textarea class="form-control" name="group-request-membership-comments" id="group-request-membership-comments"></textarea>
 
 				<?php do_action( 'bp_group_request_membership_content' ); ?>
 
-				<p><input class="btn btn-primary" type="submit" name="group-request-send" id="group-request-send" value="<?php _e( 'Send Request', 'buddypress' ); ?>" />
+				<p><input class="btn btn-primary" type="submit" name="group-request-send" id="group-request-send" value="<?php esc_attr_e( 'Send Request', 'openlab-theme' ); ?>" />
 
 					<?php wp_nonce_field( 'groups_request_membership' ); ?>
 			</form><!-- #request-membership-form -->
