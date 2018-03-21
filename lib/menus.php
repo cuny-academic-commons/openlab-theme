df42884ee6b377f2b05552bec0ff36079c250f20
Boone B Gorges <boonebgorges@gmail.com>
2018-03-20 21:57:34 -0500

    Remove Change Avatar subnav item on group admin.
    See #138.

--- lib/menus.php
+++ lib/menus.php
@@ -1006,10 +1006,6 @@ function openlab_group_admin_tabs( $group = false ) {
 
 		--><li<?php if ( 'site-details' === $current_tab ) : ?> class="current-menu-item"<?php endif; ?>><a href="<?php echo bp_get_root_domain() . '/' . bp_get_groups_root_slug() . '/' . $group->slug ?>/admin/site-details"><?php echo esc_html_x( 'Site', 'Group admin nav item', 'openlab-theme' ); ?></a></li><!--
 
-		<?php if ( ! (int) bp_get_option( 'bp-disable-avatar-uploads' ) ) : ?>
-			--><li<?php if ( 'group-avatar' == $current_tab ) : ?> class="current-menu-item"<?php endif; ?>><a href="<?php echo bp_get_root_domain() . '/' . bp_get_groups_root_slug() . '/' . $group->slug ?>/admin/group-avatar"><?php esc_html_e( 'Change Avatar', 'openlab-theme' ); ?></a></li><!--
-		<?php endif; ?>
-
 		--><li<?php if ( 'group-settings' == $current_tab ) : ?> class="current-menu-item"<?php endif; ?>><a href="<?php echo bp_get_root_domain() . '/' . bp_get_groups_root_slug() . '/' . $group->slug ?>/admin/group-settings"><?php esc_html_e( 'Privacy Settings', 'openlab-theme' ); ?></a></li>
 
 		<li class="delete-button <?php if ( 'delete-group' == $current_tab ) : ?> current-menu-item<?php endif; ?>" ><span class="fa fa-minus-circle"></span><a href="<?php echo bp_get_root_domain() . '/' . bp_get_groups_root_slug() . '/' . $group->slug ?>/admin/delete-group"><?php esc_html_e( 'Delete Portfolio', 'openlab-theme' ); ?></a></li><!--
@@ -1027,8 +1023,6 @@ function openlab_group_admin_tabs( $group = false ) {
 		?>
 
 		--><li<?php if ( 'site-details' === $current_tab ) : ?> class="current-menu-item"<?php endif; ?>><a href="<?php echo bp_get_root_domain() . '/' . bp_get_groups_root_slug() . '/' . $group->slug ?>/admin/site-details"><?php echo esc_html_x( 'Site', 'Group admin nav item', 'openlab-theme' ); ?></a></li><!--
-
-		--><li<?php if ( 'group-avatar' == $current_tab ) : ?> class="current-menu-item"<?php endif; ?>><a href="<?php echo bp_get_root_domain() . '/' . bp_get_groups_root_slug() . '/' . $group->slug ?>/admin/group-avatar"><?php _e( 'Change Avatar', 'openlab-theme' ); ?></a></li><!--
 
 		--><li<?php if ( 'group-settings' == $current_tab ) : ?> class="current-menu-item"<?php endif; ?>><a href="<?php echo bp_get_root_domain() . '/' . bp_get_groups_root_slug() . '/' . $group->slug ?>/admin/group-settings"><?php _e( 'Settings', 'openlab-theme' ); ?></a></li><!--
 
