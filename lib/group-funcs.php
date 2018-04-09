21d0d968bc24af88ca02f55987b5f07ae35b23a6
Boone B Gorges <boonebgorges@gmail.com>
2018-04-08 20:40:41 -0500

    Proper privacy filtering for WP Grade Comments private comments on group home.
    See #145.

--- lib/group-funcs.php
+++ lib/group-funcs.php
@@ -1794,6 +1794,51 @@ function openlab_show_site_posts_and_comments() {
 				'number' => '3',
 			);
 
+			/*
+			 * WP Grade Comments support.
+			 *
+			 * We reproduce the logic of olgc_get_inaccessible_comments() because there's
+			 * no way to use that function directly inside switch_to_blog().
+			 */
+			$comment__not_in = array();
+			$private_comments = get_comments( array(
+				'meta_query' => array(
+					array(
+						'key'   => 'olgc_is_private',
+						'value' => '1',
+					),
+				),
+				'status' => 'any',
+			) );
+			if ( $private_comments ) {
+				$user_id = bp_loggedin_user_id();
+
+				/*
+				 * Filter out the ones that are written by the logged-in user, as well
+				 * as those that are attached to a post that the user is the author of.
+				 */
+				foreach ( $private_comments as $private_comment ) {
+					if ( $user_id && ! empty( $private_comment->user_id ) && $user_id == $private_comment->user_id ) {
+						continue;
+					}
+
+					if ( $user_id ) {
+						$comment_post = get_post( $private_comment->comment_post_ID );
+						if ( $user_id == $comment_post->post_author ) {
+							continue;
+						}
+					}
+
+					$comment__not_in[] = $private_comment->comment_ID;
+				}
+
+				$comment__not_in = wp_parse_id_list( $comment__not_in );
+
+				if ( $comment__not_in ) {
+					$comment_args['comment__not_in'] = $comment__not_in;
+				}
+			}
+
 			$wp_comments = get_comments( $comment_args );
 
 			foreach ( $wp_comments as $wp_comment ) {
