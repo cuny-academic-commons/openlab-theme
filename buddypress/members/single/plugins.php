0b6f879c2c2820f5e87a159ffbca3454adc3f4b7
Boone B Gorges <boonebgorges@gmail.com>
2018-04-19 15:29:02 +0000

    Fix redundant member header on plugins.php pages.

--- buddypress/members/single/plugins.php
+++ buddypress/members/single/plugins.php
@@ -9,12 +9,11 @@
 	do_action( 'bp_before_member_plugin_template' );
 	?>
 
-	<h1 class="entry-title mol-title"><?php bp_displayed_user_fullname() ?>'s Profile</h1>
-	
-	<?php echo openlab_submenu_markup( 'invitations' ); ?>
+	<?php if ( bp_is_current_component( 'invite-anyone' ) ) : ?>
+		<?php echo openlab_submenu_markup( 'invitations' ); ?>
+	<?php endif; ?>
 
 	<div id="item-body" role="main">
-		<?php do_action( 'bp_before_member_body' ); ?>
 		<?php do_action( 'bp_template_content' );
 		do_action( 'bp_after_member_body' );
 		?>
