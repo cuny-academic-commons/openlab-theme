494fb2fef00ed5281666ae493ebf9a05e99bacb0
Boone B Gorges <boonebgorges@gmail.com>
2018-07-25 15:41:38 +0000

    Version bump for cache bust.

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-define( 'OPENLAB_VERSION', '1.0.0-1531941876986' );
+define( 'OPENLAB_VERSION', '1.0.0-1532533290262' );
 
 if ( ! defined( 'CSS_DEBUG' ) ) {
 	define( 'CSS_DEBUG', false );
