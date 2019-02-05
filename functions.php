6936e8799b2c1103481f6709c6af5940ac1fef31
Boone B Gorges <boonebgorges@gmail.com>
2019-02-05 16:57:57 -0600

    Asset version bump.

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-define( 'OPENLAB_VERSION', '1.1.1-beta-1548793148366' );
+define( 'OPENLAB_VERSION', '1.1.1-beta-1549407472775' );
 
 if ( ! defined( 'CSS_DEBUG' ) ) {
 	define( 'CSS_DEBUG', false );
