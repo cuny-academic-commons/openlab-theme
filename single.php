6ecaa984c79f24979ade7e09dae6e741e59eb22e
Boone B Gorges <boonebgorges@gmail.com>
2019-08-14 14:18:29 -0500

    Initial pass at styling for single posts.

--- single.php
+++ single.php
@@ -1,13 +1,41 @@
 <?php get_header(); ?>
-<div id="openlab-main-content"></div>
-<section id="content" role="main">
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<?php get_template_part('entry'); ?>
-<?php if ( ! post_password_required() ) comments_template('', true); ?>
-<?php endwhile; endif; ?>
-<footer class="footer">
-<?php get_template_part('nav', 'below-single'); ?>
-</footer>
-</section>
-<?php get_sidebar(); ?>
+
+<div id="content" class="hfeed row">
+	<?php
+	global $wp_query;
+	$post = $wp_query->post;
+	$postID = $post->ID;
+	$parent = $post->post_parent;
+	?>
+
+	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+		<div <?php post_class(); ?>>
+			<div id="openlab-main-content"  class="content-wrapper">
+				<div class="entry-title">
+					<h1><span class="profile-name"><?php the_title(); ?></span></h1>
+				</div>
+				<div class="entry-meta">
+					<?php
+					$author_id = get_the_author_meta( 'ID' );
+					printf(
+						/* translators */
+						esc_html__( 'Posted on %1$s by %2$s', 'openlab-theme' ),
+						esc_html( get_the_date( 'F j, Y' ) ),
+						bp_core_get_userlink( get_the_author_meta( 'ID' ) )
+					);
+					?>
+				</div>
+
+				<div class="entry-content"><?php the_content(); ?></div>
+			</div>
+		</div><!--hentry-->
+
+		<?php if ( ! post_password_required() ) : ?>
+			<?php comments_template( '', true ); ?>
+		<?php endif; ?>
+	<?php endwhile; endif; ?>
+
+</div><!--#content-->
+
 <?php get_footer(); ?>
