c66c19eca43bd7e25539fdf3722f98791de90cea
Boone B Gorges <boonebgorges@gmail.com>
2020-05-16 21:45:17 -0500

    Use core functions for adjacent links in help template.
    Fixes cuny-academic-commons/commons-in-a-box#221.

--- lib/help-funcs.php
+++ lib/help-funcs.php
@@ -53,15 +53,8 @@ function openlab_help_loop() {
 
 		$back_next_nav = '';
 
-		$adj_args = array(
-		'order_by' => 'menu_order',
-		'order_2nd' => 'post_date',
-		'post_type' => '"help"',
-		'return' => 'object',
-		);
-
-		$prev_post = adjacent_post_link_plus( $adj_args, '', true );
-		$next_post = adjacent_post_link_plus( $adj_args, '', false );
+		$prev_post = get_adjacent_post( false, '', true );
+		$next_post = get_adjacent_post( false, '', false );
 
 		$back_next_nav .= '<nav id="help-title-nav"><!--';
 
