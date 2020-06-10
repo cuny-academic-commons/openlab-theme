79350cbc8a700d8a678fca369f1c98854970af6f
Boone B Gorges <boonebgorges@gmail.com>
2020-06-10 10:05:12 -0500

    PHPCS fixes for docs/single/index.php.

--- buddypress/docs/single/index.php
+++ buddypress/docs/single/index.php
@@ -2,14 +2,16 @@
 
 <?php if ( bp_docs_is_doc_edit_locked() && bp_docs_current_user_can( 'edit' ) ) : ?>
 	<div class="toggleable doc-is-locked">
-		<span class="toggle-switch" id="toggle-doc-is-locked"><?php _e( 'Locked', 'bp-docs' ); ?> <span class="hide-if-no-js description"><?php _e( '(click for more info)', 'bp-docs' ); ?></span></span>
+		<span class="toggle-switch" id="toggle-doc-is-locked"><?php esc_html_e( 'Locked', 'commons-in-a-box' ); ?> <span class="hide-if-no-js description"><?php esc_html_e( '(click for more info)', 'commons-in-a-box' ); ?></span></span>
 		<div class="toggle-content">
-			<p><?php printf( __( 'This doc is currently being edited by %1$s. In order to prevent edit conflicts, only one user can edit a doc at a time.', 'bp-docs' ), bp_docs_get_current_doc_locker_name() ); ?></p>
+			<?php /* translators: name of user currently editing Doc */ ?>
+			<p><?php printf( esc_html__( 'This doc is currently being edited by %1$s. In order to prevent edit conflicts, only one user can edit a doc at a time.', 'commons-in-a-box' ), esc_html( bp_docs_get_current_doc_locker_name() ) ); ?></p>
 
 			<?php if ( is_super_admin() || bp_group_is_admin() ) : ?>
-				<p><?php printf( __( 'Please try again in a few minutes. Or, as an admin, you can <a href="%s">force cancel</a> the edit lock.', 'bp-docs' ), bp_docs_get_force_cancel_edit_lock_link() ); ?></p>
+				<?php /* translators: URL to cancel edit lock */ ?>
+				<p><?php printf( esc_html__( 'Please try again in a few minutes. Or, as an admin, you can <a href="%s">force cancel</a> the edit lock.', 'commons-in-a-box' ), esc_attr( bp_docs_get_force_cancel_edit_lock_link() ) ); ?></p>
 			<?php else : ?>
-				<p><?php _e( 'Please try again in a few minutes.', 'bp-docs' ); ?></p>
+				<p><?php esc_html_e( 'Please try again in a few minutes.', 'commons-in-a-box' ); ?></p>
 			<?php endif ?>
 		</div>
 	</div>
