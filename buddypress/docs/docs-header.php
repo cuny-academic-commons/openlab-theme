8d569f6578a40b63af2d9e1861a1dc638bac8370
Boone B Gorges <boonebgorges@gmail.com>
2020-06-10 10:02:24 -0500

    PHPCS fixes for docs-header.php.

--- buddypress/docs/docs-header.php
+++ buddypress/docs/docs-header.php
@@ -1,20 +1,14 @@
 <?php if ( bp_docs_is_existing_doc() ) : ?>
 	<div class="doc-tabs">
 		<ul>
-			<li
-			<?php
-			if ( 'single' == bp_docs_current_view() ) :
-				?>
-				 class="current"<?php endif ?>>
-				<a href="<?php echo bp_docs_get_group_doc_permalink(); ?>"><?php esc_html_e( 'Read', 'commons-in-a-box' ); ?></a>
+			<?php $li_class = 'single' === bp_docs_current_view() ? 'current' : ''; ?>
+			<li class="<?php echo esc_attr( $li_class ); ?>"
+				<a href="<?php echo esc_attr( bp_docs_get_group_doc_permalink() ); ?>"><?php esc_html_e( 'Read', 'commons-in-a-box' ); ?></a>
 			</li>
 
 			<?php if ( current_user_can( 'bp_docs_edit', bp_docs_get_current_doc()->ID ) ) : ?>
-				<li
-				<?php
-				if ( 'edit' === bp_docs_current_view() ) :
-					?>
-					 class="current"<?php endif ?>>
+				<?php $li_class = 'edit' === bp_docs_current_view() ? 'current' : ''; ?>
+				<li class="<?php echo esc_attr( $li_class ); ?>">
 					<a href="<?php echo esc_url( bp_docs_get_group_doc_permalink() . '/' . BP_DOCS_EDIT_SLUG ); ?>"><?php esc_html_e( 'Edit', 'commons-in-a-box' ); ?></a>
 				</li>
 			<?php endif ?>
