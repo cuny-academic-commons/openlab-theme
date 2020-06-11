55d9bdcb41520502756930a1f6b91363e4143137
Boone B Gorges <boonebgorges@gmail.com>
2020-06-11 14:31:24 -0500

    PHPCS for lib/plugin-hooks.php.

--- lib/plugin-hooks.php
+++ lib/plugin-hooks.php
@@ -8,14 +8,14 @@
  * Invite Anyone
  * See also: openlab/buddypress/members/single/invite-anyone.php for template overrides
  */
-require_once( get_template_directory() . '/lib/plugin-mods/invite-funcs.php' );
+require_once get_template_directory() . '/lib/plugin-mods/invite-funcs.php';
 
 /**
  * Event Organiser
  * BuddyPress Event Organiser
  */
 if ( function_exists( 'bpeo_is_action' ) ) {
-	require_once( get_template_directory() . '/lib/plugin-mods/calendar-control.php' );
+	require_once get_template_directory() . '/lib/plugin-mods/calendar-control.php';
 }
 
 /**
@@ -31,18 +31,18 @@ add_filter( 'invite_anyone_send_friend_requests_on_acceptance', '__return_false'
  * See also: mu-plugins/openlab-group-documents-privacy.php
  */
 if ( defined( 'BP_GROUP_DOCUMENTS_VERSION' ) ) {
-	require_once( get_template_directory() . '/lib/plugin-mods/files-funcs.php' );
+	require_once get_template_directory() . '/lib/plugin-mods/files-funcs.php';
 }
 
 if ( defined( 'BP_DOCS_VERSION' ) ) {
-	require_once( get_template_directory() . '/lib/plugin-mods/docs-funcs.php' );
+	require_once get_template_directory() . '/lib/plugin-mods/docs-funcs.php';
 }
 
 /**
  * BuddyPress Group Email Subscription
  * See also: openlab/buddypress/groups/single/notifications.php for template overrides
  */
-require_once( get_template_directory() . '/lib/plugin-mods/email-funcs.php' );
+require_once get_template_directory() . '/lib/plugin-mods/email-funcs.php';
 
 /**
  * Plugin: BuddyPress Group Email Subscription
@@ -59,7 +59,7 @@ function openlab_manage_members_email_status( $user_id = '', $group = '' ) {
 	global $members_template, $groups_template;
 
 	// if group admins / mods cannot manage email subscription settings, stop now!
-	if ( get_option( 'ass-admin-can-edit-email' ) == 'no' ) {
+	if ( 'no' === get_option( 'ass-admin-can-edit-email' ) ) {
 		return;
 	}
 
@@ -81,27 +81,29 @@ function openlab_manage_members_email_status( $user_id = '', $group = '' ) {
 	$user_id = (int) $user_id;
 
 	$group_url = bp_get_group_permalink( $group ) . 'admin/manage-members/email';
-	$sub_type = ass_get_group_subscription_status( $user_id, $group->id );
+	$sub_type  = ass_get_group_subscription_status( $user_id, $group->id );
 	echo '<h5>Email Status</h5>';
 
 	echo '<ul class="group-manage-members-bpges-status">';
-	echo '  <li><input name="group-manage-members-bpges-status-' . $user_id . '" type="radio" ' . checked( 'no', $sub_type, false ) . ' data-url="' . esc_url( wp_nonce_url( $group_url . '/no/' . $user_id . '/', 'ass_member_email_status' ) ) . '" value="no" /> No Email</li>';
-	echo '  <li><input name="group-manage-members-bpges-status-' . $user_id . '" type="radio" ' . checked( 'sum', $sub_type, false ) . ' data-url="' . esc_url( wp_nonce_url( $group_url . '/sum/' . $user_id . '/', 'ass_member_email_status' ) ) . '" value="sum" /> Weekly</li>';
-	echo '  <li><input name="group-manage-members-bpges-status-' . $user_id . '" type="radio" ' . checked( 'dig', $sub_type, false ) . ' data-url="' . esc_url( wp_nonce_url( $group_url . '/dig/' . $user_id . '/', 'ass_member_email_status' ) ) . '" value="dig" /> Daily</li>';
-	echo '  <li><input name="group-manage-members-bpges-status-' . $user_id . '" type="radio" ' . checked( 'supersub', $sub_type, false ) . ' data-url="' . esc_url( wp_nonce_url( $group_url . '/supersub/' . $user_id . '/', 'ass_member_email_status' ) ) . '" value="supersub" /> All Email</li>';
+	echo '  <li><input name="group-manage-members-bpges-status-' . esc_attr( $user_id ) . '" type="radio" ' . checked( 'no', $sub_type, false ) . ' data-url="' . esc_url( wp_nonce_url( $group_url . '/no/' . $user_id . '/', 'ass_member_email_status' ) ) . '" value="no" /> ' . esc_html__( 'No Email', 'commons-in-a-box' ) . '</li>';
+	echo '  <li><input name="group-manage-members-bpges-status-' . esc_attr( $user_id ) . '" type="radio" ' . checked( 'sum', $sub_type, false ) . ' data-url="' . esc_url( wp_nonce_url( $group_url . '/sum/' . $user_id . '/', 'ass_member_email_status' ) ) . '" value="sum" /> ' . esc_html__( 'Weekly', 'commons-in-a-box' ) . '</li>';
+	echo '  <li><input name="group-manage-members-bpges-status-' . esc_attr( $user_id ) . '" type="radio" ' . checked( 'dig', $sub_type, false ) . ' data-url="' . esc_url( wp_nonce_url( $group_url . '/dig/' . $user_id . '/', 'ass_member_email_status' ) ) . '" value="dig" /> ' . esc_html__( 'Daily', 'commons-in-a-box' ) . '</li>';
+	echo '  <li><input name="group-manage-members-bpges-status-' . esc_attr( $user_id ) . '" type="radio" ' . checked( 'supersub', $sub_type, false ) . ' data-url="' . esc_url( wp_nonce_url( $group_url . '/supersub/' . $user_id . '/', 'ass_member_email_status' ) ) . '" value="supersub" /> ' . esc_html__( 'All Email', 'commons-in-a-box' ) . '</li>';
 
 	echo '</ul>';
 
-	wp_enqueue_script( 'openlab-bpges-js', get_template_directory_uri() . '/js/bpges.js', array( 'jquery' ), openlab_get_asset_version() );
+	wp_enqueue_script( 'openlab-bpges-js', get_template_directory_uri() . '/js/bpges.js', array( 'jquery' ), openlab_get_asset_version(), true );
 }
-
 remove_action( 'bp_group_manage_members_admin_item', 'ass_manage_members_email_status' );
 add_action( 'bp_group_manage_members_admin_item', 'openlab_manage_members_email_status' );
 
 // remove status from group profile pages
-add_action( 'bp_actions', function() {
-	remove_action( 'bp_group_header_meta', 'ass_group_subscribe_button' );
-} );
+add_action(
+	'bp_actions',
+	function() {
+		remove_action( 'bp_group_header_meta', 'ass_group_subscribe_button' );
+	}
+);
 
 
 /**
@@ -128,20 +130,16 @@ add_action( 'bbp_before_group_forum_display', 'openlab_forum_tabs_output' );
  * Injectiong bootstrap classes into BBPress comment textarea field
  *
  * @param type $output
- * @param type $args
- * @param type $post_content
  * @return type
  */
-function openlab_custom_bbp_content( $output, $args, $post_content ) {
-
+function openlab_custom_bbp_content( $output ) {
 	if ( strpos( $output, 'textarea' ) !== false ) {
 		$output = str_replace( 'wp-editor-area', 'form-control', $output );
 	}
 
 	return $output;
 }
-
-add_filter( 'bbp_get_the_content', 'openlab_custom_bbp_content', 10, 3 );
+add_filter( 'bbp_get_the_content', 'openlab_custom_bbp_content', 10 );
 
 /**
  * Plugin: BBPress
@@ -154,7 +152,7 @@ function openlab_bbp_pagination( $pag_args ) {
 
 	$pag_args['prev_text'] = __( '<i class="fa fa-angle-left"></i>' );
 	$pag_args['next_text'] = __( '<i class="fa fa-angle-right"></i>' );
-	$pag_args['type'] = 'list';
+	$pag_args['type']      = 'list';
 
 	return $pag_args;
 }
@@ -182,21 +180,16 @@ add_filter( 'bbp_get_forum_pagination_links', 'openlab_bbp_paginatin_custom_mark
  * Injecting bootstrap and site standard button classes into subscription toggle button
  *
  * @param type $html
- * @param type $r
- * @param type $user_id
- * @param type $topic_id
  * @return type
  */
-function openlab_style_bbp_subscribe_link( $html, $r, $user_id, $topic_id ) {
-
+function openlab_style_bbp_subscribe_link( $html ) {
 	if ( ! bbp_is_single_topic() ) {
 		$html = str_replace( 'class="subscription-toggle"', 'class="subscription-toggle btn btn-primary btn-margin btn-margin-top no-deco"', $html );
 	}
 
 	return $html;
 }
-
-add_filter( 'bbp_get_user_subscribe_link', 'openlab_style_bbp_subscribe_link', 10, 4 );
+add_filter( 'bbp_get_user_subscribe_link', 'openlab_style_bbp_subscribe_link', 10 );
 
 /**
  * More generous cap mapping for bbPress topic posting.
@@ -209,29 +202,29 @@ function openlab_bbp_map_group_forum_meta_caps( $caps = array(), $cap = '', $use
 	}
 	switch ( $cap ) {
 		// If user is a group mmember, allow them to create content.
-		case 'read_forum' :
-		case 'publish_replies' :
-		case 'publish_topics' :
-		case 'read_hidden_forums' :
-		case 'read_private_forums' :
+		case 'read_forum':
+		case 'publish_replies':
+		case 'publish_topics':
+		case 'read_hidden_forums':
+		case 'read_private_forums':
 			if ( bbp_group_is_member() || bbp_group_is_mod() || bbp_group_is_admin() ) {
 				$caps = array( 'exist' );
 			}
 			break;
 		// If user is a group mod ar admin, map to participate cap.
-		case 'moderate' :
-		case 'edit_topic' :
-		case 'edit_reply' :
-		case 'view_trash' :
-		case 'edit_others_replies' :
-		case 'edit_others_topics' :
+		case 'moderate':
+		case 'edit_topic':
+		case 'edit_reply':
+		case 'view_trash':
+		case 'edit_others_replies':
+		case 'edit_others_topics':
 			if ( bbp_group_is_mod() || bbp_group_is_admin() ) {
 				$caps = array( 'exist' );
 			}
 			break;
 		// If user is a group admin, allow them to delete topics and replies.
-		case 'delete_topic' :
-		case 'delete_reply' :
+		case 'delete_topic':
+		case 'delete_reply':
 			if ( bbp_group_is_admin() ) {
 				$caps = array( 'exist' );
 			}
@@ -290,7 +283,7 @@ add_filter( 'posts_results', 'openlab_bbp_force_forums_to_public', 10, 2 );
  * Otherwise activity is not posted.
  */
 function openlab_bbp_force_site_public_to_1( $public, $site_id ) {
-	if ( 1 == $site_id ) {
+	if ( bp_get_root_blog_id() === (int) $site_id ) {
 		$public = 1;
 	}
 	return $public;
@@ -302,9 +295,10 @@ add_filter( 'bbp_is_site_public', 'openlab_bbp_force_site_public_to_1', 10, 2 );
  * Handle feature toggling for groups.
  */
 function openlab_group_feature_toggle( $group_id ) {
+	// phpcs:disable WordPress.Security.NonceVerification.Missing
 	// Discussion.
-	$enable_forum = ! empty( $_POST['openlab-edit-group-forum'] );
-	$group = groups_get_group( $group_id );
+	$enable_forum        = ! empty( $_POST['openlab-edit-group-forum'] );
+	$group               = groups_get_group( $group_id );
 	$group->enable_forum = $enable_forum;
 	$group->save();
 
@@ -315,8 +309,8 @@ function openlab_group_feature_toggle( $group_id ) {
 	}
 
 	// Docs.
-	$enable_docs   = ! empty( $_POST['openlab-edit-group-docs'] );
-	$docs_settings = bp_docs_get_group_settings( $group_id );
+	$enable_docs                   = ! empty( $_POST['openlab-edit-group-docs'] );
+	$docs_settings                 = bp_docs_get_group_settings( $group_id );
 	$docs_settings['group-enable'] = (int) $enable_docs;
 	groups_update_groupmeta( $group_id, 'bp-docs', $docs_settings );
 
@@ -335,6 +329,7 @@ function openlab_group_feature_toggle( $group_id ) {
 	} else {
 		groups_update_groupmeta( $group_id, 'calendar_is_disabled', '1' );
 	}
+	// phpcs:enable WordPress.Security.NonceVerification.Missing
 }
 add_action( 'groups_settings_updated', 'openlab_group_feature_toggle' );
 
@@ -356,7 +351,7 @@ function openlab_is_forum_enabled_for_group( $group_id = false ) {
 		return false;
 	}
 
-	$disable = (bool) groups_get_groupmeta( $group_id, 'openlab_disable_forum' );
+	$disable  = (bool) groups_get_groupmeta( $group_id, 'openlab_disable_forum' );
 	$forum_id = groups_get_groupmeta( $group_id, 'forum_id' );
 
 	if ( $disable || ! $forum_id ) {
@@ -415,7 +410,7 @@ add_filter( 'bbp_is_forum_public', 'openlab_enforce_forum_privacy', 10, 2 );
  * This can cause a costly tree rebuild. See bbPress #1799. See OL #1663,
  */
 function openlab_prevent_bbp_recounts( $r ) {
-	if ( bbp_get_group_forums_root_id() == $r['forum_id'] ) {
+	if ( bbp_get_group_forums_root_id() === (int) $r['forum_id'] ) {
 		$r['forum_id'] = 0;
 	}
 
@@ -424,14 +419,14 @@ function openlab_prevent_bbp_recounts( $r ) {
 add_filter( 'bbp_after_update_forum_parse_args', 'openlab_prevent_bbp_recounts' );
 
 function openlab_prevent_bbpress_from_recalculating_group_root_reply_count( $id ) {
-	$group_root = bbp_get_group_forums_root_id();
+	$group_root      = bbp_get_group_forums_root_id();
 	$group_root_post = get_post( $group_root );
 	if ( ! $group_root_post ) {
 		return $id;
 	}
 
 	$group_root_parent = $group_root_post->post_parent;
-	if ( $group_root != $id && $group_root_parent != $id ) {
+	if ( (int) $group_root !== (int) $id && (int) $group_root_parent !== (int) $id ) {
 		return $id;
 	}
 
@@ -445,7 +440,7 @@ function openlab_prevent_bbpress_from_recalculating_group_root_reply_count( $id 
 		}
 	}
 
-	if ( 'bbp_update_forum_reply_count' == $caller ) {
+	if ( 'bbp_update_forum_reply_count' === $caller ) {
 		return 0;
 	}
 
@@ -475,7 +470,7 @@ function openlab_remove_bbpress_forum_title( $title ) {
 	}
 
 	// Pretty cool technique.
-	$is_display_forums = false;
+	$is_display_forums        = false;
 	$is_single_forum_template = false;
 	// phpcs:disable
 	foreach ( debug_backtrace() as $db ) {
@@ -512,7 +507,7 @@ function openlab_log_out_social_accounts() {
 	}
 
 	$user_id = get_current_user_id();
-	$social = get_user_meta( $user_id, 'social_commenter', true );
+	$social  = get_user_meta( $user_id, 'social_commenter', true );
 
 	if ( 'true' === $social ) {
 		// Make sure there's no last_activity, so the user doesn't show in directories.
@@ -527,7 +522,7 @@ function openlab_log_out_social_accounts() {
 
 		// Log out and redirect.
 		wp_clear_auth_cookie();
-		wp_redirect( '/' );
+		wp_safe_redirect( '/' );
 		die();
 	}
 }
@@ -541,18 +536,21 @@ add_action( 'init', 'openlab_log_out_social_accounts', 0 );
 /**
  * Remove bp-braille native settings UI.
  */
-add_action( 'bp_init', function() {
-	if ( ! class_exists( '\HardG\BpBraille\Plugin' ) ) {
-		return;
-	}
+add_action(
+	'bp_init',
+	function() {
+		if ( ! class_exists( '\HardG\BpBraille\Plugin' ) ) {
+			return;
+		}
 
-	$instance = \HardG\BpBraille\Plugin::get_instance();
+		$instance = \HardG\BpBraille\Plugin::get_instance();
 
-	if ( isset( $instance->messages->settings ) ) {
-		remove_action( 'bp_core_general_settings_before_submit', array( $instance->messages->settings, 'render' ) );
-	}
+		if ( isset( $instance->messages->settings ) ) {
+			remove_action( 'bp_core_general_settings_before_submit', array( $instance->messages->settings, 'render' ) );
+		}
 
-	if ( isset( $instance->groups->settings ) ) {
-		remove_action( 'bp_after_group_details_admin', array( $instance->groups->settings, 'render' ) );
+		if ( isset( $instance->groups->settings ) ) {
+			remove_action( 'bp_after_group_details_admin', array( $instance->groups->settings, 'render' ) );
+		}
 	}
-} );
+);
