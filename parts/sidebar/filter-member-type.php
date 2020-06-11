224b1ed05b678af42a3351f9d432c3405d9c4836
Boone B Gorges <boonebgorges@gmail.com>
2020-06-11 13:49:30 -0500

    PHPCS for parts/sidebar/filter-member-type.php.

--- parts/sidebar/filter-member-type.php
+++ parts/sidebar/filter-member-type.php
@@ -4,11 +4,11 @@ $member_type_slug = urldecode( openlab_get_current_filter( 'member_type' ) );
 
 <div class="custom-select">
 	<label for="portfolio-user-member-type-select" class="sr-only"><?php echo esc_html_e( 'Select: User Type', 'commons-in-a-box' ); ?></label>
-	<select name="member_type" class="last-select <?php echo $user_color; ?>-text" id="portfolio-user-member-type-select">
-		<option value='' <?php selected( '', $member_type_slug ) ?>><?php esc_html_e( 'User Type', 'commons-in-a-box' ); ?></option>
+	<select name="member_type" class="last-select" id="portfolio-user-member-type-select">
+		<option value='' <?php selected( '', $member_type_slug ); ?>><?php esc_html_e( 'User Type', 'commons-in-a-box' ); ?></option>
 		<?php foreach ( cboxol_get_member_types() as $member_type ) : ?>
-			<option value='<?php echo esc_attr( $member_type->get_slug() ); ?>' <?php selected( $member_type->get_slug(), $member_type_slug ) ?>><?php echo esc_html( $member_type->get_label( 'singular' ) ); ?></option>
+			<option value='<?php echo esc_attr( $member_type->get_slug() ); ?>' <?php selected( $member_type->get_slug(), $member_type_slug ); ?>><?php echo esc_html( $member_type->get_label( 'singular' ) ); ?></option>
 		<?php endforeach; ?>
-		<option value='all' <?php selected( 'all', $member_type_slug ) ?>>All</option>
+		<option value='all' <?php selected( 'all', $member_type_slug ); ?>><?php esc_html_e( 'All', 'commons-in-a-box' ); ?></option>
 	</select>
 </div>
