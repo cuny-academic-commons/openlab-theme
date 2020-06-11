e4a7f56d7731411163a2f0f6403645a8b18da528
Boone B Gorges <boonebgorges@gmail.com>
2020-06-11 12:44:12 -0500

    PHPCS for 404.php.

--- 404.php
+++ 404.php
@@ -4,30 +4,19 @@
  *
  */
 
-  get_header(); ?>
+get_header(); ?>
 
-  <div id="content" class="hfeed">
-  			<div <?php post_class(); ?>>
-            	<?php cuny_404(); ?>
-            </div><!--hentry-->
-  </div><!--#content-->
+<div id="content" class="hfeed">
+	<div <?php post_class(); ?>>
+		<div class="post hentry">
+			<div class="entry-title">
+				<h1><?php esc_html_e( 'Page Not Found', 'commons-in-a-box' ); ?></h1>
+			</div>
 
- <?php get_footer();
+			<div id="openlab-main-content" class="entry-content">
+				<p><?php esc_html_e( 'The page you requested could not be found. Please use the menu above to find the page you need.', 'commons-in-a-box' ); ?></p>
+			</div><!-- end .entry-content -->
 
-function cuny_404() { ?>
-
-	<div class="post hentry">
-
-		<div class="entry-title">
-			<h1><?php esc_html_e( 'Page Not Found', 'commons-in-a-box' ); ?></h1>
-		</div>
-
-		<div id="openlab-main-content" class="entry-content">
-			<p><?php esc_html_e( 'The page you requested could not be found. Please use the menu above to find the page you need.', 'commons-in-a-box' ); ?></p>
-
-		</div><!-- end .entry-content -->
-
-	</div><!-- end .postclass -->
-
-<?php
-}
+		</div><!-- end .post -->
+	</div>
+</div><!--#content-->
