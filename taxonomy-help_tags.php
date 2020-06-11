e2f1456b82234707da09021fa8cc616458fbbf28
Boone B Gorges <boonebgorges@gmail.com>
2020-06-11 12:45:29 -0500

    PHPCS for taxonomy-help_tags.php.

--- taxonomy-help_tags.php
+++ taxonomy-help_tags.php
@@ -7,12 +7,13 @@
 /**begin layout**/
 get_header(); ?>
 	<div id="content" class="hfeed">
-            <div class="col-sm-18 col-xs-24">
-                <div id="openlab-main-content" class="content-wrapper">
-                    <?php openlab_help_tags_loop(); ?>
-                </div>
-            </div>
+			<div class="col-sm-18 col-xs-24">
+				<div id="openlab-main-content" class="content-wrapper">
+					<?php openlab_help_tags_loop(); ?>
+				</div>
+			</div>
 			<?php get_template_part( 'parts/sidebar/help' ); ?>
 	</div>
-<?php get_footer();
+<?php
+get_footer();
 /**end layout**/
