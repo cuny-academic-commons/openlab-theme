840a65916b8bbc1c4a24253dff0282eeeba37ab6
Boone B Gorges <boonebgorges@gmail.com>
2020-06-11 13:57:16 -0500

    PHPCS for parts/sidebar/filter-sort.php.

--- parts/sidebar/filter-sort.php
+++ parts/sidebar/filter-sort.php
@@ -4,8 +4,8 @@ $option_value = openlab_get_current_filter( 'sort' );
 <div class="custom-select">
 	<label for="sequence-select" class="sr-only"><?php echo esc_html_e( 'Select: Order', 'commons-in-a-box' ); ?></label>
 	<select name="sort" class="last-select" id="sequence-select">
-		<option <?php selected( $option_value, 'alphabetical' ) ?> value='alphabetical'><?php esc_html_e( 'Alphabetical', 'commons-in-a-box' ); ?></option>
-		<option <?php selected( $option_value, 'newest' ) ?>  value='newest'><?php esc_html_e( 'Newest', 'commons-in-a-box' ); ?></option>
-		<option <?php selected( $option_value, 'active' ) ?> value='active'><?php esc_html_e( 'Last Active', 'commons-in-a-box' ); ?></option>
+		<option <?php selected( $option_value, 'alphabetical' ); ?> value='alphabetical'><?php esc_html_e( 'Alphabetical', 'commons-in-a-box' ); ?></option>
+		<option <?php selected( $option_value, 'newest' ); ?>  value='newest'><?php esc_html_e( 'Newest', 'commons-in-a-box' ); ?></option>
+		<option <?php selected( $option_value, 'active' ); ?> value='active'><?php esc_html_e( 'Last Active', 'commons-in-a-box' ); ?></option>
 	</select>
 </div>
