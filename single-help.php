e17a6e2512dcdb67e0afc956ec5b6d41157b8ea0
Boone B Gorges <boonebgorges@gmail.com>
2020-06-11 12:45:59 -0500

    PHPCBF for single-help.php.

--- single-help.php
+++ single-help.php
@@ -6,15 +6,15 @@ Template Name: Help
 /**begin layout**/
 get_header(); ?>
 	<div id="content" class="hfeed row">
-            <div class="col-sm-18 col-xs-24 col-help">
-                <div id="openlab-main-content" class="content-wrapper">
-                    <?php openlab_help_loop(); ?>
-                </div>
-            </div>
+			<div class="col-sm-18 col-xs-24 col-help">
+				<div id="openlab-main-content" class="content-wrapper">
+					<?php openlab_help_loop(); ?>
+				</div>
+			</div>
 
 			<?php get_template_part( 'parts/sidebar/help' ); ?>
 
-            </div>
+			</div>
 <?php get_footer();
 /**end layout**/
 ?>
