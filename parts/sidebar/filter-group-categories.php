fc05868902378f4c776e2e9e0ca5fff8e5deeda9
Boone B Gorges <boonebgorges@gmail.com>
2020-06-11 13:56:40 -0500

    PHPCS for parts/sidebar/filter-group-categories.php.

--- parts/sidebar/filter-group-categories.php
+++ parts/sidebar/filter-group-categories.php
@@ -9,10 +9,10 @@ $option_value_bpcgc = openlab_get_current_filter( 'group_category' );
 	<div class="custom-select">
 		<label for="bp-group-categories-select" class="sr-only"><?php echo esc_html_e( 'Select: Category', 'commons-in-a-box' ); ?></label>
 		<select name="cat" class="last-select" id="bp-group-categories-select">
-			<option value="" <?php selected( '', $option_value_bpcgc ) ?>><?php esc_html_e( 'Category', 'commons-in-a-box' ); ?></option>
+			<option value="" <?php selected( '', $option_value_bpcgc ); ?>><?php esc_html_e( 'Category', 'commons-in-a-box' ); ?></option>
 			<option value='cat_all' <?php selected( 'cat_all', $option_value_bpcgc ); ?>><?php esc_html_e( 'All', 'commons-in-a-box' ); ?></option>
-			<?php foreach ( $group_terms as $term ) : ?>
-				<option value="<?php echo esc_attr( $term->slug ); ?>" <?php selected( $option_value_bpcgc, $term->slug ); ?>><?php echo esc_html( $term->name ); ?></option>
+			<?php foreach ( $group_terms as $the_term ) : ?>
+				<option value="<?php echo esc_attr( $the_term->slug ); ?>" <?php selected( $option_value_bpcgc, $the_term->slug ); ?>><?php echo esc_html( $the_term->name ); ?></option>
 			<?php endforeach; ?>
 		</select>
 	</div>
