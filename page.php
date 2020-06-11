cd71006b11234272f871abb56b541775a9fed6e3
Boone B Gorges <boonebgorges@gmail.com>
2020-06-11 12:20:04 -0500

    PHPCS for page.php.

--- page.php
+++ page.php
@@ -2,11 +2,6 @@
 
 <div id="content" class="hfeed row">
 	<?php
-	global $wp_query;
-	$post = $wp_query->post;
-	$postID = $post->ID;
-	$parent = $post->post_parent;
-
 	$show_about_sidebar = cboxol_is_brand_page( 'about' ) || cboxol_is_brand_page( 'terms-of-use' ) || cboxol_is_brand_page( 'contact-us' );
 
 	if ( $show_about_sidebar ) {
@@ -14,30 +9,31 @@
 	}
 	?>
 
-	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+	<?php
+	if ( have_posts() ) :
+		while ( have_posts() ) :
+			the_post();
+			?>
 
-			<?php //my-<group> pages should not be displaying this ?>
-			<?php if ( ! strstr( get_the_title(), 'My' ) ) :  ?>
-				<div <?php post_class( 'col-sm-18 col-xs-24' ); ?>>
-					<div id="openlab-main-content"  class="content-wrapper">
-						<div class="entry-title">
-							<h1><span class="profile-name"><?php the_title(); ?></span></h1>
+			<div <?php post_class( 'col-sm-18 col-xs-24' ); ?>>
+				<div id="openlab-main-content"  class="content-wrapper">
+					<div class="entry-title">
+						<h1><span class="profile-name"><?php the_title(); ?></span></h1>
 
-							<?php if ( $show_about_sidebar ) :  ?>
-								<div class="directory-title-meta">
-									<button data-target="#sidebar-mobile" class="mobile-toggle direct-toggle pull-right visible-xs" type="button">
-										<span class="sr-only"><?php esc_html_e( 'Toggle navigation', 'commons-in-a-box' ); ?></span>
-										<span class="icon-bar"></span>
-										<span class="icon-bar"></span>
-										<span class="icon-bar"></span>
-									</button>
-								</div>
-							<?php endif; ?>
-						</div>
-						<div class="entry-content"><?php the_content(); ?></div>
+						<?php if ( $show_about_sidebar ) : ?>
+							<div class="directory-title-meta">
+								<button data-target="#sidebar-mobile" class="mobile-toggle direct-toggle pull-right visible-xs" type="button">
+									<span class="sr-only"><?php esc_html_e( 'Toggle navigation', 'commons-in-a-box' ); ?></span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+									<span class="icon-bar"></span>
+								</button>
+							</div>
+						<?php endif; ?>
 					</div>
-				</div><!--hentry-->
-			<?php endif; ?>
+					<div class="entry-content"><?php the_content(); ?></div>
+				</div>
+			</div><!--hentry-->
 
 			<?php
 		endwhile;
