5660c356798fd21fe509e4ecba7a46b1db2fe46e
Boone B Gorges <boonebgorges@gmail.com>
2020-06-11 12:53:08 -0500

    PHPCS for parts/sidebar/activity-events-feed.php.

--- parts/sidebar/activity-events-feed.php
+++ parts/sidebar/activity-events-feed.php
@@ -1,26 +1,25 @@
 <div class="sidebar-widget" id="group-member-portfolio-sidebar-widget">
-	<h2 class="sidebar-header">Upcoming Events</h2>
+	<h2 class="sidebar-header"><?php esc_html_e( 'Upcoming Events', 'commons-in-a-box' ); ?></h2>
 
 	<div class="sidebar-block">
 
 		<ul class="group-event-activity inline-element-list group-data-list sidebar-sublinks">
 			<?php if ( ! empty( $events ) ) : ?>
 				<?php
-				$_post = $GLOBALS['post'];
-				foreach ( $events as $post ) {
+				foreach ( $events as $event_post ) {
 					?>
-					<li class="bpeo-upcoming-event-<?php echo esc_attr( $post->ID ); ?>">
-						<a class="bpeo-upcoming-event-title" href="<?php echo esc_url( apply_filters( 'eventorganiser_calendar_event_link', get_permalink( $post->ID ), $post->ID ) ); ?>"><?php echo esc_html( $post->post_title ); ?></a><br />
-						<span class="bpeo-upcoming-event-date"><?php echo date( 'M j, Y', strtotime( $post->StartDate ) ); ?></span>&nbsp;<span class="bpeo-upcoming-event-time"><?php echo date( 'g:ia', strtotime( $post->StartTime ) ); ?></span>
+					<li class="bpeo-upcoming-event-<?php echo esc_attr( $event_post->ID ); ?>">
+						<a class="bpeo-upcoming-event-title" href="<?php echo esc_url( apply_filters( 'eventorganiser_calendar_event_link', get_permalink( $event_post->ID ), $event_post->ID ) ); ?>"><?php echo esc_html( $event_post->post_title ); ?></a><br />
+						<?php // phpcs:ignore WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase ?>
+						<span class="bpeo-upcoming-event-date"><?php echo esc_html( gmdate( 'M j, Y', strtotime( $event_post->StartDate ) ) ); ?></span>&nbsp;<span class="bpeo-upcoming-event-time"><?php echo esc_html( gmdate( 'g:ia', strtotime( $event_post->StartTime ) ) ); ?></span>
 					</li>
 					<?php
 				}
-				$GLOBALS['post'] = $_post;
 				?>
 
-			<?php else : // ! empty( $events ) ?>
-				<li><?php _e( 'No upcoming events found.', 'bp-event-organiser' ); ?></li>
-			<?php endif; // ! empty( $events ) ?>
+			<?php else : ?>
+				<li><?php esc_html_e( 'No upcoming events found.', 'commons-in-a-box' ); ?></li>
+			<?php endif; ?>
 		</ul>
 
 	</div>
