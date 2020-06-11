30f80e91d71747a9474995c556ec3655ce92978b
Boone B Gorges <boonebgorges@gmail.com>
2020-06-11 12:12:46 -0500

    PHPCBF for spacing.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,5 @@
 module.exports = function (grunt) {
-    require('jit-grunt')(grunt);
+	require( 'jit-grunt' )( grunt );
 
 	colorSchemes = {
 		blue: {
@@ -52,78 +52,80 @@ module.exports = function (grunt) {
 		}
 	};
 
-	var lessTasks = {}
-	var colorSchemePath = '';
+	var lessTasks        = {}
+	var colorSchemePath  = '';
 	var colorSchemeTasks = [];
 	for ( color in colorSchemes ) {
-		colorSchemePath = 'css/color-schemes/' + color + '.css';
-		toolbarColorSchemePath = 'css/color-schemes/toolbar-' + color + '.css';
-		lessTasks[ color ] = {
+		colorSchemePath                                    = 'css/color-schemes/' + color + '.css';
+		toolbarColorSchemePath                             = 'css/color-schemes/toolbar-' + color + '.css';
+		lessTasks[ color ]                                 = {
 			options: {
 				modifyVars: colorSchemes[ color ],
 				optimization: 2
 			},
 			files: {}
 		}
-		lessTasks[ color ].files[ colorSchemePath ] = 'style.less';
+		lessTasks[ color ].files[ colorSchemePath ]        = 'style.less';
 		lessTasks[ color ].files[ toolbarColorSchemePath ] = 'less/openlab-toolbar.less';
 		colorSchemeTasks.push( 'less:' + color );
 	};
 
 	var timestamp = new Date().getTime();
 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        concat: {
-            options: {
-                separator: ';'
-            },
-            vendor: {
-                src: ['js/bootstrap.min.js',
-                    'node_modules/jcarousellite/jcarousellite.js',
-                    'js/easyaccordion.js',
-                    'js/jquery.easing.1.3.js',
-                    'js/jquery.mobile.customized.min.js',
-                    'js/camera.mod.js',
-                    'js/jQuery.succinct.mod.js',
-                    'js/detect-zoom.js'],
-                dest: 'js/dist/vendor.js'
-            },
-        },
-        less: lessTasks,
-        watch: {
-            styles: {
-                files: ['*.less', 'less/*.less'], // which files to watch
-                tasks: ['less'],
-                options: {
-                    nospawn: true
-                }
-            },
-            scripts: {
-                files: ['js/*.js'],
-                tasks: ['concat'],
-                options: {
-                    nospawn: true
-                }
-            }
-        },
-				setPHPConstant: {
-					version: {
-						constant: 'OPENLAB_VERSION',
-						value: '<%= pkg.version %>-' + timestamp,
-						file: 'functions.php'
+	grunt.initConfig(
+		{
+			pkg: grunt.file.readJSON( 'package.json' ),
+			concat: {
+				options: {
+					separator: ';'
+				},
+				vendor: {
+					src: ['js/bootstrap.min.js',
+					'node_modules/jcarousellite/jcarousellite.js',
+					'js/easyaccordion.js',
+					'js/jquery.easing.1.3.js',
+					'js/jquery.mobile.customized.min.js',
+					'js/camera.mod.js',
+					'js/jQuery.succinct.mod.js',
+					'js/detect-zoom.js'],
+					dest: 'js/dist/vendor.js'
+				},
+			},
+			less: lessTasks,
+			watch: {
+				styles: {
+					files: ['*.less', 'less/*.less'], // which files to watch
+					tasks: ['less'],
+					options: {
+						nospawn: true
 					}
 				},
-    });
-    grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-php-set-constant');
+				scripts: {
+					files: ['js/*.js'],
+					tasks: ['concat'],
+					options: {
+						nospawn: true
+					}
+				}
+			},
+			setPHPConstant: {
+				version: {
+					constant: 'OPENLAB_VERSION',
+					value: '<%= pkg.version %>-' + timestamp,
+					file: 'functions.php'
+				}
+			},
+		}
+	);
+	grunt.loadNpmTasks( 'grunt-contrib-less' );
+	grunt.loadNpmTasks( 'grunt-contrib-uglify' );
+	grunt.loadNpmTasks( 'grunt-contrib-concat' );
+	grunt.loadNpmTasks( 'grunt-contrib-watch' );
+	grunt.loadNpmTasks( 'grunt-php-set-constant' );
 
 		var taskList = ['concat', 'watch', 'less', 'setPHPConstant'];
-		taskList = taskList.concat( colorSchemeTasks );
+		taskList     = taskList.concat( colorSchemeTasks );
 
 		var defaultTasks = ['concat', 'less', 'setPHPConstant'];
-    grunt.registerTask('default', defaultTasks);
+	grunt.registerTask( 'default', defaultTasks );
 };
