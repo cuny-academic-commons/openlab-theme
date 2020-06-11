fd26e53b86258ddcd99839b95d5ead36466d5deb
Boone B Gorges <boonebgorges@gmail.com>
2020-06-11 14:33:16 -0500

    PHPCS for lib/buddypress.php.

--- lib/buddypress.php
+++ lib/buddypress.php
@@ -4,7 +4,7 @@
 add_action( 'wp_enqueue_scripts', 'openlab_bp_enqueue_scripts', 20 );
 
 // BP-specific breadcrumb overrides.
-add_filter( 'openlab_page_crumb', 'openlab_page_crumb_overrides', 10, 2 );
+add_filter( 'openlab_page_crumb', 'openlab_page_crumb_overrides', 10 );
 
 /**
  * Enqueue front-end script that are BP-dependent.
@@ -19,35 +19,44 @@ function openlab_bp_enqueue_scripts() {
 	}
 
 	if ( bp_is_group_create() || bp_is_group_admin_page() ) {
-		wp_enqueue_script( 'openlab-group-create', get_template_directory_uri() . '/js/group-create.js', array( 'jquery' ), $ver );
+		wp_enqueue_script( 'openlab-group-create', get_template_directory_uri() . '/js/group-create.js', array( 'jquery' ), $ver, true );
 
-		wp_localize_script( 'openlab-group-create', 'OLGroupCreate', array(
-			'strings' => array(
-				'externalFeedsFieldLabelComments' => __( 'Comments:', 'commons-in-a-box' ),
-				'externalFeedsFieldLabelPosts' => __( 'Posts:', 'commons-in-a-box' ),
-				'externalFeedsFound' => __( 'We found the following feed URLs for your external site, which we\'ll use to pull posts and comments into your activity stream.', 'commons-in-a-box' ),
-				'externalFeedsNotFound' => __( 'We couldn\'t find any feed URLs for your external site, which we use to pull posts and comments into your activity stream. If your site has feeds, you may enter the URLs below.', 'commons-in-a-box' ),
-				'fieldCannotBeBlank' => __( 'This field cannot be blank.', 'commons-in-a-box' ),
-				'incompleteCrop' => __( 'Please crop your image before continuing.', 'commons-in-a-box' ),
-			),
-		) );
+		wp_localize_script(
+			'openlab-group-create',
+			'OLGroupCreate',
+			array(
+				'strings' => array(
+					'externalFeedsFieldLabelComments' => __( 'Comments:', 'commons-in-a-box' ),
+					'externalFeedsFieldLabelPosts'    => __( 'Posts:', 'commons-in-a-box' ),
+					'externalFeedsFound'              => __( 'We found the following feed URLs for your external site, which we\'ll use to pull posts and comments into your activity stream.', 'commons-in-a-box' ),
+					'externalFeedsNotFound'           => __( 'We couldn\'t find any feed URLs for your external site, which we use to pull posts and comments into your activity stream. If your site has feeds, you may enter the URLs below.', 'commons-in-a-box' ),
+					'fieldCannotBeBlank'              => __( 'This field cannot be blank.', 'commons-in-a-box' ),
+					'incompleteCrop'                  => __( 'Please crop your image before continuing.', 'commons-in-a-box' ),
+				),
+			)
+		);
 	}
 
 	if ( bp_is_register_page() ) {
-		wp_enqueue_script( 'openlab-registration', get_template_directory_uri() . '/js/register.js', array( 'jquery', 'parsley' ), $ver );
+		wp_enqueue_script( 'openlab-registration', get_template_directory_uri() . '/js/register.js', array( 'jquery', 'parsley' ), $ver, true );
 
-		wp_localize_script('openlab-registration', 'OLReg', array(
-			'account_type_field' => xprofile_get_field_id_from_name( 'Account Type ' ),
-			'limited_email_domains' => get_site_option( 'limited_email_domains' ),
-			'post_data' => $_POST,
-			'strings' => array(
-				'completeSignUp' => esc_html__( 'Complete Sign Up', 'commons-in-a-box' ),
-				'dashChecking' => esc_html__( '&mdash; Checking', 'commons-in-a-box' ),
-				'dashOK' => esc_html__( '&mdash; OK!', 'commons-in-a-box' ),
-				'enterEmailAddressToContinue' => esc_html__( 'Enter Email Address to Continue', 'commons-in-a-box' ),
-				'dashInvalidCode' => esc_html__( '&mdash; Invalid code', 'commons-in-a-box' ),
-			),
-		) );
+		wp_localize_script(
+			'openlab-registration',
+			'OLReg',
+			array(
+				'account_type_field'    => xprofile_get_field_id_from_name( 'Account Type ' ),
+				'limited_email_domains' => get_site_option( 'limited_email_domains' ),
+				// phpcs:ignore WordPress.Security.NonceVerification.Missing
+				'post_data'             => $_POST,
+				'strings'               => array(
+					'completeSignUp'              => esc_html__( 'Complete Sign Up', 'commons-in-a-box' ),
+					'dashChecking'                => esc_html__( '&mdash; Checking', 'commons-in-a-box' ),
+					'dashOK'                      => esc_html__( '&mdash; OK!', 'commons-in-a-box' ),
+					'enterEmailAddressToContinue' => esc_html__( 'Enter Email Address to Continue', 'commons-in-a-box' ),
+					'dashInvalidCode'             => esc_html__( '&mdash; Invalid code', 'commons-in-a-box' ),
+				),
+			)
+		);
 	}
 }
 
@@ -61,17 +70,17 @@ function openlab_bp_enqueue_scripts() {
  * @param array  $args  Argument array.
  * @return string
  */
-function openlab_page_crumb_overrides( $crumb, $args ) {
+function openlab_page_crumb_overrides( $crumb ) {
 	global $post, $bp;
 
 	if ( bp_is_groups_directory() ) {
 		$crumb = '';
 
 		$group_type_slug = bp_get_current_group_directory_type();
-		$group_type = cboxol_get_group_type( $group_type_slug );
+		$group_type      = cboxol_get_group_type( $group_type_slug );
 		if ( ! is_wp_error( $group_type ) ) {
 			$type_directory_url = bp_get_group_type_directory_permalink( $group_type->get_slug() );
-			$crumb = sprintf(
+			$crumb              = sprintf(
 				'<a href="%s">%s</a>',
 				esc_url( $type_directory_url ),
 				esc_html( $group_type->get_label( 'plural' ) )
@@ -84,7 +93,7 @@ function openlab_page_crumb_overrides( $crumb, $args ) {
 
 		$group = groups_get_current_group();
 
-		$group_type = cboxol_get_group_group_type( $group->id );
+		$group_type         = cboxol_get_group_group_type( $group->id );
 		$type_directory_url = '';
 		if ( ! is_wp_error( $group_type ) ) {
 			$type_directory_url = bp_get_group_type_directory_permalink( $group_type->get_slug() );
