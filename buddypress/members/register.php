134218e6f06f3d10138ca01baad4bd91756c28ff
Boone B Gorges <boonebgorges@gmail.com>
2020-06-15 16:44:07 -0500

    PHPCS for buddypress/members/register.php.

--- buddypress/members/register.php
+++ buddypress/members/register.php
@@ -21,6 +21,7 @@
 			$led[] = sprintf( '<span class="limited-email-domain">' . esc_html( $d ) . '</span>' );
 		}
 		$limited_email_domains_message = sprintf(
+			// translators: list of allowed email domains
 			esc_html__( 'Allowed email domains: %s', 'commons-in-a-box' ),
 			implode( ', ', $led )
 		);
@@ -35,12 +36,12 @@
 		<div id="openlab-main-content"></div>
 
 		<div class="entry-title">
-			<h1><?php _e( 'Create an Account', 'commons-in-a-box' ); ?></h1>
+			<h1><?php esc_html_e( 'Create an Account', 'commons-in-a-box' ); ?></h1>
 		</div>
 
 		<form action="" name="signup_form" id="signup_form" class="standard-form form-panel" method="post" enctype="multipart/form-data" data-parsley-trigger="blur">
 
-			<?php if ( 'request-details' == bp_get_current_signup_step() ) : ?>
+			<?php if ( 'request-details' === bp_get_current_signup_step() ) : ?>
 
 				<div class="panel panel-default">
 					<div class="panel-heading semibold"><?php esc_html_e( 'Account Details', 'commons-in-a-box' ); ?></div>
@@ -48,6 +49,7 @@
 
 						<?php do_action( 'template_notices' ); ?>
 
+						<?php // translators: site name ?>
 						<p><?php printf( esc_html__( 'Registering for %s is easy. Just fill in the fields below and we\'ll get a new account set up for you in no time.', 'commons-in-a-box' ), esc_html( $site_name ) ); ?></p>
 
 						<?php do_action( 'bp_before_account_details_fields' ); ?>
@@ -57,6 +59,15 @@
 							<div class="form-group">
 								<label class="control-label" for="signup_username"><?php esc_html_e( 'Username', 'commons-in-a-box' ); ?> <?php esc_html_e( '(required)', 'commons-in-a-box' ); ?> <?php esc_html_e( '(lowercase & no special characters)', 'commons-in-a-box' ); ?></label>
 								<?php do_action( 'bp_signup_username_errors' ); ?>
+								<?php
+								$login_check_url = add_query_arg(
+									array(
+										'action' => 'openlab_unique_login_check',
+										'login'  => '{value}',
+									),
+									$ajaxurl
+								);
+								?>
 								<input
 									class="form-control"
 									type="text"
@@ -67,17 +78,7 @@
 									data-parsley-nospecialchars
 									data-parsley-required
 									data-parsley-minlength="4"
-									data-parsley-remote="
-									<?php
-									echo add_query_arg(
-										array(
-											'action' => 'openlab_unique_login_check',
-											'login'  => '{value}',
-										),
-										$ajaxurl
-									);
-									?>
-									"
+									data-parsley-remote="<?php echo esc_attr( $login_check_url ); ?>"
 									data-parsley-remote-message="<?php esc_attr_e( 'That username is already taken.', 'commons-in-a-box' ); ?>"
 								/>
 							</div>
@@ -86,6 +87,7 @@
 								<label class="control-label" for="signup_email"><?php esc_html_e( 'Email Address (required)', 'commons-in-a-box' ); ?> <?php
 								if ( $limited_email_domains_message ) :
 									?>
+									<?php // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
 									<div class="email-requirements"><?php echo $limited_email_domains_message; ?></div><?php endif; ?></label>
 								<?php do_action( 'bp_signup_email_errors' ); ?>
 								<input
@@ -119,7 +121,7 @@
 							</div>
 
 							<div data-parsley-children-should-match class="form-group">
-								<label class="control-label" for="signup_password"><?php _e( 'Choose a Password', 'commons-in-a-box' ); ?> <?php _e( '(required)', 'commons-in-a-box' ); ?></label>
+								<label class="control-label" for="signup_password"><?php esc_html_e( 'Choose a Password', 'commons-in-a-box' ); ?> <?php esc_html_e( '(required)', 'commons-in-a-box' ); ?></label>
 								<?php do_action( 'bp_signup_password_errors' ); ?>
 								<div class="password-field">
 									<input
@@ -138,7 +140,7 @@
 									<div id="password-strength-notice" class="password-strength-notice"></div>
 								</div>
 
-								<label class="control-label" for="signup_password_confirm"><?php _e( 'Confirm Password', 'commons-in-a-box' ); ?> <?php _e( '(required)', 'commons-in-a-box' ); ?></label>
+								<label class="control-label" for="signup_password_confirm"><?php esc_html_e( 'Confirm Password', 'commons-in-a-box' ); ?> <?php esc_html_e( '(required)', 'commons-in-a-box' ); ?></label>
 								<?php do_action( 'bp_signup_password_confirm_errors' ); ?>
 								<input
 									class="form-control password-field"
@@ -172,10 +174,11 @@
 
 								<p><?php esc_html_e( 'Your responses in the form fields below will be displayed on your profile page, which is open to the public. You can always add, edit, or remove information at a later date.', 'commons-in-a-box' ); ?></p>
 
-								<?php /* @todo Abstract selector? */ ?>
 								<?php
+								// phpcs:disable WordPress.Security.NonceVerification.Missing
 								$selected_account_type = isset( $_POST['account-type'] ) ? wp_unslash( $_POST['account-type'] ) : '';
 								$entered_signup_code   = isset( $_POST['account-type-signup-code'] ) ? wp_unslash( $_POST['account-type-signup-code'] ) : '';
+								// phpcs:enable WordPress.Security.NonceVerification.Missing
 								?>
 								<div class="editfield form-group account-type-select-ui">
 									<?php do_action( 'bp_account_type_errors' ); ?>
@@ -191,12 +194,7 @@
 										</div>
 
 										<div class="col-md-8">
-											<input class="form-control" name="account-type-signup-code" id="account-type-signup-code" placeholder="<?php esc_attr_e( 'Please enter a sign up code', 'commons-in-a-box' ); ?>" value="
-																																									 <?php
-																																										if ( $entered_signup_code ) {
-																																											echo esc_attr( $entered_signup_code ); }
-																																										?>
-											" />
+											<input class="form-control" name="account-type-signup-code" id="account-type-signup-code" placeholder="<?php esc_attr_e( 'Please enter a sign up code', 'commons-in-a-box' ); ?>" value="<?php echo esc_attr( $entered_signup_code ); ?>" />
 										</div>
 
 										<div class="col-md-8 signup-code-message" id="signup-code-message"></div>
@@ -206,6 +204,7 @@
 								<div id="openlab-profile-fields"></div>
 
 								<?php
+								// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 								echo cboxol_get_academic_unit_selector(
 									array(
 										'entity_type' => 'user',
@@ -226,13 +225,14 @@
 				<?php do_action( 'bp_before_registration_submit_buttons' ); ?>
 
 				<p class="sign-up-terms">
+					<?php // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
 					<?php echo $registration_form_settings['confirmationText']; ?>
 				</p>
 
 				<p id="submitSrMessage" class="sr-only submit-alert" aria-live="polite"></p>
 
 				<div class="submit">
-					<input type="submit" name="signup_submit" id="signup_submit" class="btn btn-primary btn-disabled" value="<?php _e( 'Please Complete Required Fields', 'buddypress' ); ?>" />
+					<input type="submit" name="signup_submit" id="signup_submit" class="btn btn-primary btn-disabled" value="<?php esc_html_e( 'Please Complete Required Fields', 'commons-in-a-box' ); ?>" />
 				</div>
 
 				<?php do_action( 'bp_after_registration_submit_buttons' ); ?>
@@ -241,18 +241,18 @@
 
 			<?php endif; // request-details signup step ?>
 
-			<?php if ( 'completed-confirmation' == bp_get_current_signup_step() ) : ?>
+			<?php if ( 'completed-confirmation' === bp_get_current_signup_step() ) : ?>
 
 				<div class="panel panel-default">
-					<div class="panel-heading semibold"><?php _e( 'Sign Up Complete!', 'buddypress' ); ?></div>
+					<div class="panel-heading semibold"><?php esc_html_e( 'Sign Up Complete!', 'commons-in-a-box' ); ?></div>
 					<div class="panel-body">
 
 						<?php do_action( 'template_notices' ); ?>
 
 						<?php if ( bp_registration_needs_activation() ) : ?>
-							<p class="bp-template-notice updated no-margin no-margin-bottom"><?php _e( 'You have successfully created your account! To begin using this site you will need to activate your account via the email we have just sent to your address.', 'buddypress' ); ?></p>
+							<p class="bp-template-notice updated no-margin no-margin-bottom"><?php esc_html_e( 'You have successfully created your account! To begin using this site you will need to activate your account via the email we have just sent to your address.', 'commons-in-a-box' ); ?></p>
 						<?php else : ?>
-							<p class="bp-template-notice updated no-margin no-margin-bottom"><?php _e( 'You have successfully created your account! Please log in using the username and password you have just created.', 'buddypress' ); ?></p>
+							<p class="bp-template-notice updated no-margin no-margin-bottom"><?php esc_html_e( 'You have successfully created your account! Please log in using the username and password you have just created.', 'commons-in-a-box' ); ?></p>
 						<?php endif; ?>
 
 					</div>
