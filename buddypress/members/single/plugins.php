4321124ffe618262cd4845431bf10cf869e2f99b
Boone B Gorges <boonebgorges@gmail.com>
2020-06-15 16:55:03 -0500

    PHPCS for buddypress/members/single/plugins.php.

--- buddypress/members/single/plugins.php
+++ buddypress/members/single/plugins.php
@@ -4,20 +4,21 @@
  *
  * */
 /* * begin layout* */
-	global $bp;
 
-	do_action( 'bp_before_member_plugin_template' );
+do_action( 'bp_before_member_plugin_template' );
 ?>
 
-	<?php if ( bp_is_current_component( 'invite-anyone' ) ) : ?>
-		<?php echo openlab_submenu_markup( 'invitations' ); ?>
-	<?php endif; ?>
+<?php if ( bp_is_current_component( 'invite-anyone' ) ) : ?>
+	<?php // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+	<?php echo openlab_submenu_markup( 'invitations' ); ?>
+<?php endif; ?>
 
-	<div id="item-body" role="main">
-		<?php
-		do_action( 'bp_template_content' );
-		do_action( 'bp_after_member_body' );
-		?>
-	</div><!-- #item-body -->
+<div id="item-body" role="main">
 	<?php
-	do_action( 'bp_after_member_plugin_template' );
+	do_action( 'bp_template_content' );
+	do_action( 'bp_after_member_body' );
+	?>
+</div><!-- #item-body -->
+
+<?php
+do_action( 'bp_after_member_plugin_template' );
