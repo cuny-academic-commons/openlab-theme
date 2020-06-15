9c08e8d2a11918f0ac86c18536dc170aa91b6006
Boone B Gorges <boonebgorges@gmail.com>
2020-06-15 12:56:53 -0500

    PHPCS for parts/pageslopenlab-calendar-upcaming.php.

--- parts/pages/openlab-calendar-upcoming.php
+++ parts/pages/openlab-calendar-upcoming.php
@@ -4,33 +4,32 @@
 			<div class="submenu-text pull-left bold">Calendar:</div>
 			<ul class="nav nav-inline">
 				<?php foreach ( $menu_items as $item ) : ?>
-					<li class="<?php echo $item['class']; ?>" id="<?php echo $item['slug']; ?>-groups-li"><a href="<?php echo $item['link']; ?>"><?php echo $item['name']; ?></a></li>
+					<li class="<?php echo esc_attr( $item['class'] ); ?>" id="<?php echo esc_attr( $item['slug'] ); ?>-groups-li"><a href="<?php echo esc_attr( $item['link'] ); ?>"><?php echo esc_html( $item['name'] ); ?></a></li>
 				<?php endforeach; ?>
 			</ul>
 		</div>
 
-		<h2><?php echo __( 'Upcoming Events', 'bp-event-organiser' ); ?></h2>
+		<h2><?php esc_html_e( 'Upcoming Events', 'commons-in-a-box' ); ?></h2>
 		<?php if ( ! empty( $events ) ) : ?>
 			<ul class="bpeo-upcoming-events">
 				<?php
-				$_post = $GLOBALS['post'];
-				foreach ( $events as $post ) {
+				foreach ( $events as $event_post ) {
 					?>
-					<li class="bpeo-upcoming-event-<?php echo esc_attr( $post->ID ); ?>">
+					<li class="bpeo-upcoming-event-<?php echo esc_attr( $event_post->ID ); ?>">
 						<div class="bpeo-upcoming-event-datetime">
-							<span class="bpeo-upcoming-event-date"><?php echo date( 'M j, Y', strtotime( $post->StartDate ) ); ?></span> &middot;&nbsp; <span class="bpeo-upcoming-event-time"><?php echo date( 'g:ia', strtotime( $post->StartTime ) ); ?></span>
+							<?php // phpcs:ignore WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase ?>
+							<span class="bpeo-upcoming-event-date"><?php echo esc_attr( gmdate( 'M j, Y', strtotime( $event_post->StartDate ) ) ); ?></span> &middot;&nbsp; <span class="bpeo-upcoming-event-time"><?php echo esc_html( gmdate( 'g:ia', strtotime( $event_post->StartTime ) ) ); ?></span>
 						</div>
 
-						<a class="bpeo-upcoming-event-title" href="<?php echo esc_url( apply_filters( 'eventorganiser_calendar_event_link', get_permalink( $post->ID ), $post->ID ) ); ?>"><?php echo esc_html( $post->post_title ); ?></a>
+						<a class="bpeo-upcoming-event-title" href="<?php echo esc_url( apply_filters( 'eventorganiser_calendar_event_link', get_permalink( $event_post->ID ), $post->ID ) ); ?>"><?php echo esc_html( $event_post->post_title ); ?></a>
 
 					</li>
 					<?php
 				}
-				$GLOBALS['post'] = $_post;
 				?>
 			</ul>
-		<?php else : // ! empty( $events ) ?>
-			<p><?php _e( 'No upcoming events found.', 'bp-event-organiser' ); ?></p>
-		<?php endif; // ! empty( $events ) ?>
+		<?php else : ?>
+			<p><?php esc_html_e( 'No upcoming events found.', 'commons-in-a-box' ); ?></p>
+		<?php endif; ?>
 	</div>
 </div>
