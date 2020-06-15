827625365491392ea9efbc9557b354c2169cad31
Boone B Gorges <boonebgorges@gmail.com>
2020-06-15 16:47:26 -0500

    PHPCS for buddypress/members/members-loop.php.

--- buddypress/members/members-loop.php
+++ buddypress/members/members-loop.php
@@ -1,5 +1,5 @@
 <?php /* Querystring is set via AJAX in _inc/ajax.php - bp_dtheme_object_filter() */ ?>
-<?php global $bp; ?>
+
 <?php do_action( 'bp_before_members_loop' ); ?>
 
 <?php if ( bp_has_members( bp_ajax_querystring( 'members' ) ) ) : ?>
@@ -7,58 +7,54 @@
 	<?php do_action( 'bp_before_directory_members_list' ); ?>
 
 	<div id="friend-list" class="item-list group-list row">
-	<?php
-	while ( bp_members() ) :
-		bp_the_member();
-		?>
+	<?php while ( bp_members() ) : ?>
+		<?php bp_the_member(); ?>
 
 		<div class="group-item col-md-8 col-xs-12">
 			<div class="group-item-wrapper">
 				<div class="row info-row">
 					<div class="item-avatar col-sm-9 col-xs-7">
-						<a href="<?php bp_member_permalink(); ?>"><img class="img-responsive" src ="
-															  <?php
-																echo bp_core_fetch_avatar(
-																	array(
-																		'item_id' => bp_get_member_user_id(),
-																		'object'  => 'member',
-																		'type'    => 'full',
-																		'html'    => false,
-																	)
-																);
-																?>
-																									" alt="<?php esc_attr( bp_get_member_name() ); ?>"/></a>
+						<?php
+						$user_avatar = bp_core_fetch_avatar(
+							array(
+								'item_id' => bp_get_member_user_id(),
+								'object'  => 'member',
+								'type'    => 'full',
+								'html'    => false,
+							)
+						);
+						?>
+						<a href="<?php bp_member_permalink(); ?>"><img class="img-responsive" src ="<?php echo esc_attr( $user_avatar ); ?>" alt="<?php esc_attr( bp_get_member_name() ); ?>"/></a>
 					</div>
 
-			<div class="item col-sm-15 col-xs-17">
-				<h5 class="item-title"><a class="no-deco" href="<?php bp_member_permalink(); ?>"><?php bp_member_name(); ?></a></h5>
+					<div class="item col-sm-15 col-xs-17">
+						<h5 class="item-title"><a class="no-deco" href="<?php bp_member_permalink(); ?>"><?php bp_member_name(); ?></a></h5>
 
-					<?php if ( bp_get_member_latest_update() ) : ?>
+						<?php if ( bp_get_member_latest_update() ) : ?>
+							<span class="update"> - <?php bp_member_latest_update( 'length=10' ); ?></span>
+						<?php endif; ?>
 
-						<span class="update"> - <?php bp_member_latest_update( 'length=10' ); ?></span>
+						<?php if ( bp_current_action() !== 'my-friends' ) : ?>
+							<div class="timestamp"><span class="fa fa-undo"></span> <?php bp_member_last_active(); ?></div>
+						<?php endif; ?>
 
-					<?php endif; ?>
+						<?php do_action( 'bp_directory_members_actions' ); ?>
 
-								<?php if ( bp_current_action() !== 'my-friends' ) : ?>
-									<div class="timestamp"><span class="fa fa-undo"></span> <?php bp_member_last_active(); ?></div>
-								<?php endif; ?>
-								<?php do_action( 'bp_directory_members_actions' ); ?>
+						<?php do_action( 'bp_directory_members_item' ); ?>
 
-				<?php do_action( 'bp_directory_members_item' ); ?>
-
-				<?php
-				 /***
-				  * If you want to show specific profile fields here you can,
-				  * but it'll add an extra query for each member in the loop
-				  * (only one regardless of the number of fields you show):
-				  *
-				  * bp_member_profile_data( 'field=the field name' );
-				  */
-				?>
-								</div>
-						</div>
+						<?php
+						/**
+						 * If you want to show specific profile fields here you can,
+						 * but it'll add an extra query for each member in the loop
+						 * (only one regardless of the number of fields you show):
+						 *
+						 * bp_member_profile_data( 'field=the field name' );
+						 */
+						?>
 					</div>
-		</div>
+				</div><!-- .row -->
+			</div><!-- .group-item-wrapper -->
+		</div><!-- .group-item -->
 
 	<?php endwhile; ?>
 	</div>
@@ -70,6 +66,7 @@
 	<div id="pag-bottom" class="pagination">
 
 		<div class="pagination-links" id="member-dir-pag-bottom">
+			<?php // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
 			<?php echo openlab_members_pagination_links(); ?>
 		</div>
 
@@ -78,7 +75,7 @@
 <?php else : ?>
 
 	<div id="message" class="info">
-		<p><?php _e( 'Sorry, no members were found.', 'buddypress' ); ?></p>
+		<p><?php esc_html_e( 'Sorry, no members were found.', 'commons-in-a-box' ); ?></p>
 	</div>
 
 <?php endif; ?>
