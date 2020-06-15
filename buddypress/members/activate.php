ce26f816ebaaf31c8e10f42bac1b54dd5172c2b1
Boone B Gorges <boonebgorges@gmail.com>
2020-06-15 16:37:39 -0500

    PHPCS for buddypress/members.
    See cuny-academic-commons/commons-in-a-box#

--- buddypress/members/activate.php
+++ buddypress/members/activate.php
@@ -14,7 +14,7 @@
 					<?php if ( bp_account_was_activated() ) : ?>
 
 						<div class="panel-heading">
-							<?php _e( 'Account Activated', 'buddypress' ); ?>
+							<?php esc_html_e( 'Account Activated', 'commons-in-a-box' ); ?>
 						</div>
 
 						<div class="panel-body">
@@ -22,30 +22,31 @@
 
 							<?php do_action( 'bp_before_activate_content' ); ?>
 
+							<?php // phpcs:ignore WordPress.Security.NonceVerification.Recommended ?>
 							<?php if ( isset( $_GET['e'] ) ) : ?>
-								<p class="bp-template-notice updated no-margin no-margin-bottom"><?php _e( 'Your account was activated successfully! Your account details have been sent to you in a separate email.', 'buddypress' ); ?></p>
+								<p class="bp-template-notice updated no-margin no-margin-bottom"><?php esc_html_e( 'Your account was activated successfully! Your account details have been sent to you in a separate email.', 'commons-in-a-box' ); ?></p>
 							<?php else : ?>
-								<p class="bp-template-notice updated no-margin no-margin-bottom"><?php _e( 'Your account was activated successfully! You can now log in with the username and password you provided when you signed up.', 'buddypress' ); ?></p>
+								<p class="bp-template-notice updated no-margin no-margin-bottom"><?php esc_html_e( 'Your account was activated successfully! You can now log in with the username and password you provided when you signed up.', 'commons-in-a-box' ); ?></p>
 							<?php endif; ?>
 						</div>
 
 					<?php else : ?>
 
 						<div class="panel-heading">
-							<?php _e( 'Activate your Account', 'buddypress' ); ?>
+							<?php esc_html_e( 'Activate your Account', 'commons-in-a-box' ); ?>
 						</div>
 
 						<div class="panel-body">
 							<?php do_action( 'bp_before_activate_content' ); ?>
 
-							<p><?php _e( 'Please provide a valid activation key.', 'buddypress' ); ?></p>
+							<p><?php esc_html_e( 'Please provide a valid activation key.', 'commons-in-a-box' ); ?></p>
 
 							<form action="" method="post" class="standard-form form" id="activation-form">
-								<label for="key"><?php _e( 'Activation Key:', 'buddypress' ); ?></label>
+								<label for="key"><?php esc_html_e( 'Activation Key:', 'commons-in-a-box' ); ?></label>
 								<input class="form-control" type="text" name="key" id="key" value="<?php echo esc_attr( bp_get_current_activation_key() ); ?>" />
 
 								<p class="submit">
-									<input class="btn btn-primary btn-margin btn-margin-top" type="submit" name="submit" value="<?php _e( 'Activate', 'buddypress' ); ?> &#xf138;" />
+									<input class="btn btn-primary btn-margin btn-margin-top" type="submit" name="submit" value="<?php esc_html_e( 'Activate', 'commons-in-a-box' ); ?> &#xf138;" />
 								</p>
 							</form>
 						</div>
