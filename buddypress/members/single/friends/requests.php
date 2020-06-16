49dfb6722f9fa0d6dd3d9634e9fc8a03eb623457
Boone B Gorges <boonebgorges@gmail.com>
2020-06-15 21:33:48 -0500

    PHPCS for buddypress/members/single/friends/requests.php.

--- buddypress/members/single/friends/requests.php
+++ buddypress/members/single/friends/requests.php
@@ -1,30 +1,25 @@
 <?php do_action( 'bp_before_member_friend_requests_content' ); ?>
 
 <?php if ( bp_has_members( 'include=' . bp_get_friendship_requests() . '&per_page=0' ) ) : ?>
-
-
 	<div id="friend-list" class="item-list group-list">
-		<?php
-		while ( bp_members() ) :
-			bp_the_member();
-			?>
+		<?php while ( bp_members() ) : ?>
+			<?php bp_the_member(); ?>
 
 			<div class="group-item col-sm-8 col-xs-12">
 				<div class="group-item-wrapper">
 					<div class="row info-row">
 						<div class="item-avatar col-sm-9 col-xs-7">
-							<a href="<?php bp_member_permalink(); ?>"><img class="img-responsive" src ="
-																  <?php
-																	echo bp_core_fetch_avatar(
-																		array(
-																			'item_id' => bp_get_member_user_id(),
-																			'object'  => 'member',
-																			'type'    => 'full',
-																			'html'    => false,
-																		)
-																	);
-																	?>
-																										" alt="<?php echo $group->name; ?>"/></a>
+							<?php
+							$user_avatar = bp_core_fetch_avatar(
+								array(
+									'item_id' => bp_get_member_user_id(),
+									'object'  => 'member',
+									'type'    => 'full',
+									'html'    => false,
+								)
+							);
+							?>
+							<a href="<?php bp_member_permalink(); ?>"><img class="img-responsive" src="<?php echo esc_attr( $user_avatar ); ?>" alt="<?php echo esc_attr( $group->name ); ?>"/></a>
 						</div>
 
 						<div class="item col-sm-15 col-xs-17">
@@ -41,8 +36,8 @@
 						<?php do_action( 'bp_friend_requests_item' ); ?>
 
 						<div class="action">
-							<a class="button accept btn btn-primary link-btn btn-xs" href="<?php bp_friend_accept_request_link(); ?>"><?php _e( 'Accept', 'buddypress' ); ?></a> &nbsp;
-							<a class="button reject btn btn-primary link-btn btn-xs" href="<?php bp_friend_reject_request_link(); ?>"><?php _e( 'Reject', 'buddypress' ); ?></a>
+							<a class="button accept btn btn-primary link-btn btn-xs" href="<?php bp_friend_accept_request_link(); ?>"><?php esc_html_e( 'Accept', 'commons-in-a-box' ); ?></a> &nbsp;
+							<a class="button reject btn btn-primary link-btn btn-xs" href="<?php bp_friend_reject_request_link(); ?>"><?php esc_html_e( 'Reject', 'commons-in-a-box' ); ?></a>
 
 							<?php do_action( 'bp_friend_requests_item_action' ); ?>
 						</div>
@@ -60,7 +55,7 @@
 
 	<div id="message" class="info group-list row">
 		<div class="col-md-24">
-			<p class="bold"><?php _e( 'You have no pending friendship requests.', 'buddypress' ); ?></p>
+			<p class="bold"><?php esc_html_e( 'You have no pending friendship requests.', 'commons-in-a-box' ); ?></p>
 		</div>
 	</div>
 
