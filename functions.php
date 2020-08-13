2306245827adb87993a79f96394691278f6fe10e
Boone B Gorges <boonebgorges@gmail.com>
2020-08-13 11:18:26 -0500

    openlab-theme: Enqueue jquery-migrate.
    This ensures compatibility with WP 5.5.
    
    See cuny-academic-commons/commons-in-a-box#273.

--- functions.php
+++ functions.php
@@ -191,7 +191,7 @@ function openlab_load_scripts() {
 
 		wp_register_script( 'select2', $stylesheet_dir_uri . '/js/select2.min.js', array( 'jquery' ), $ver, true );
 
-		$utility_deps = array( 'jquery', 'select2', 'hyphenator-js' );
+		$utility_deps = array( 'jquery', 'jquery-migrate', 'select2', 'hyphenator-js' );
 		wp_register_script( 'utility', $stylesheet_dir_uri . '/js/utility.js', $utility_deps, $ver, true );
 
 		wp_enqueue_script( 'utility' );
