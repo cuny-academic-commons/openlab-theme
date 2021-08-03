fc115042cd7c141490529e1be65fe8d7ea6adef8
Boone B Gorges <boonebgorges@gmail.com>
2021-08-03 09:37:39 -0500

    1.3.0 version bumps, continued.

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-define( 'OPENLAB_VERSION', '1.3.0-beta1-1627484355371' );
+define( 'OPENLAB_VERSION', '1.3.0-1628001441711' );
 
 if ( ! defined( 'CSS_DEBUG' ) ) {
 	define( 'CSS_DEBUG', false );
