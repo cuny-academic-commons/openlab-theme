e88741cda2a2ca145bec87eedfb7c82dc02a4060
r-a-y <r-a-y@gmx.com>
2022-06-02 17:58:07 -0700

    Fix messsages pagination.
    See https://cboxopenlab.org/groups/help-forum/forum/topic/users-cannot-access-any-page-of-paginated-ol-messages-except-the-first-page/

--- lib/member-funcs.php
+++ lib/member-funcs.php
@@ -537,13 +537,10 @@ add_action( 'bp_before_member_body', 'openlab_member_header' );
 function openlab_messages_pagination() {
 	global $messages_template;
 
-	$page_arg = '%#%';
-
 	if ( (int) $messages_template->total_thread_count && (int) $messages_template->pag_num ) {
 		$pagination = paginate_links(
 			array(
 				'base'      => add_query_arg(
-					$page_arg,
 					array(
 						'mpage' => '%#%',
 					)
