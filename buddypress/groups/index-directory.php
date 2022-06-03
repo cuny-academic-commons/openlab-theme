fa118955be5b1063ad5ecf919d2e82b89301398d
r-a-y <r-a-y@gmx.com>
2022-06-02 18:35:38 -0700

    Redirect to Portfolios page if not on a valid group directory type.

--- buddypress/groups/index-directory.php
+++ buddypress/groups/index-directory.php
@@ -1,7 +1,13 @@
 <?php
+$current_type = bp_get_current_group_directory_type();
+
+// Redirect to Portfolios if we're not on a valid directory type.
+if ( empty( $current_type ) ) {
+	bp_core_redirect( bp_get_group_type_directory_permalink( 'portfolio' ) );
+}
+
 get_header();
 
-$current_type = bp_get_current_group_directory_type();
 $type_object  = cboxol_get_group_type( $current_type );
 
 $can_create = is_user_logged_in() && bp_user_can_create_groups();
