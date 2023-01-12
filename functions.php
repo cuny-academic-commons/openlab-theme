2278640e271f186036c6b2bbae02d762ed398926
Boone B Gorges <boonebgorges@gmail.com>
2023-01-12 15:18:42 -0600

    1.4.0-beta2 version bumps.

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
-define( 'OPENLAB_VERSION', '1.4.0-beta1-1669929551751' );
+define( 'OPENLAB_VERSION', '1.4.0-beta2-1673558304258' );
 
 if ( ! defined( 'CSS_DEBUG' ) ) {
 	define( 'CSS_DEBUG', false );
