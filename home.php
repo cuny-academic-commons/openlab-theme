eed47049ebc7635662821611e789e0a11def908e
Boone B Gorges <boonebgorges@gmail.com>
2023-07-06 13:04:17 -0500

    On home page, <h1> should always be site title.
    The 'Hello' message in the logged-in login box is now a span.
    
    See cuny-academic-commons/commons-in-a-box#388.

--- home.php
+++ home.php
@@ -8,6 +8,8 @@
 get_header();
 ?>
 
+<h1 class="site-title sr-only"><?php bloginfo( 'name' ); ?></h1>
+
 <div id="openlab-main-content" class="clearfix row-home-top" role="main">
 	<div class="no-gutter no-gutter-right login">
 		<div id="cuny_openlab_jump_start">
