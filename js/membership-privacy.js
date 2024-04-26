e7b491f12ff648963afcea4a5d0d619ac1b6c574
Boone B Gorges <boonebgorges@gmail.com>
2024-04-26 17:06:48 -0500

    PHPCS formatting.

--- js/membership-privacy.js
+++ js/membership-privacy.js
@@ -5,29 +5,35 @@
  */
 jQuery( document ).ready(
 	function($) {
-		$(document).on( 'change', 'input#membership_privacy', function(e) {
-			let groupId = $(this).attr('data-group_id');
-			let isPrivate = $(this).is(':checked');
+		$( document ).on(
+			'change',
+			'input#membership_privacy',
+			function(e) {
+				let groupId   = $( this ).attr( 'data-group_id' );
+				let isPrivate = $( this ).is( ':checked' );
 
-			$.ajax({
-				url: ajaxurl,
-				method: 'POST',
-				dataType: 'json',
-				data: {
-					'action': 'openlab_update_member_group_privacy',
-					'group_id': groupId,
-					'is_private': isPrivate
-				},
-				beforeSend: function() {
-					// Disable checkbox
-					$('input#membership_privacy').attr('disabled', true );
-				},
-				complete: function() {
-					// Enable checkbox
-					$('input#membership_privacy').attr('disabled', false );
-				}
-			});
-		});
+				$.ajax(
+					{
+						url: ajaxurl,
+						method: 'POST',
+						dataType: 'json',
+						data: {
+							'action': 'openlab_update_member_group_privacy',
+							'group_id': groupId,
+							'is_private': isPrivate
+						},
+						beforeSend: function() {
+							// Disable checkbox
+							$( 'input#membership_privacy' ).attr( 'disabled', true );
+						},
+						complete: function() {
+							// Enable checkbox
+							$( 'input#membership_privacy' ).attr( 'disabled', false );
+						}
+					}
+				);
+			}
+		);
 	},
 	(jQuery)
 );
