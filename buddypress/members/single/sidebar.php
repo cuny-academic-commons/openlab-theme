c58cd763c75166feda272c42a9b78b597a1ca79f
Boone B Gorges <boonebgorges@gmail.com>
2024-07-10 15:37:15 -0500

    Remove unused code.
    See cuny-academic-commons/commons-in-a-box#458.

--- buddypress/members/single/sidebar.php
+++ buddypress/members/single/sidebar.php
@@ -1,11 +1,4 @@
 <?php
-// Get the displayed user's base domain
-// This is required because the my-* pages aren't really displayed user pages from BP's
-// point of view
-$dud = bp_displayed_user_domain();
-if ( ! $dud ) {
-	$dud = bp_loggedin_user_domain(); // will always be the logged in user on my-*
-}
 
 if ( ( bp_is_user_activity() || ! bp_current_component() ) && ! ( strpos( $post->post_name, 'my-' ) > -1 ) ) {
 	$mobile_hide = true;
