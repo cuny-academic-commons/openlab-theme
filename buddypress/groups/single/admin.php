ce9fc1bdf063cc114640a36c32692926a3b42390
Boone B Gorges <boonebgorges@gmail.com>
2024-07-11 08:36:56 -0500

    More text updates for portfolio profile link UI.
    See cuny-academic-commons/commons-in-a-box#476.

--- buddypress/groups/single/admin.php
+++ buddypress/groups/single/admin.php
@@ -60,7 +60,7 @@ $private_users = openlab_get_private_members_of_group( bp_get_group_id() );
 						<fieldset class="portfolio-profile-link-wrapper">
 							<legend><?php esc_html_e( 'Portfolio Link on my OpenLab Profile', 'ocommons-in-a-box' ); ?></legend>
 
-							<p><?php esc_html_e( 'You can choose to show a link to your Portfolio on your OpenLab Profile page by checking the box below. If your Display Name is different from your real name, but you want to use your real name on your Portfolio, you may wish to leave this unchecked.', 'commons-in-a-box' ); ?></p>
+							<p><?php esc_html_e( 'You can choose to show a link to your Portfolio on your Profile page by checking the box below.', 'commons-in-a-box' ); ?></p>
 
 							<input name="portfolio-profile-link" id="portfolio-profile-link-toggle" <?php checked( openlab_show_portfolio_link_on_user_profile( openlab_get_user_id_from_portfolio_group_id( bp_get_current_group_id() ) ) ); ?> type="checkbox" name="portfolio-profile-link-toggle" value="1" /> <label for="portfolio-profile-link-toggle"><?php esc_html_e( 'Show link to my Portfolio on my public Profile', 'commons-in-a-box' ); ?></label>
 
