b2fcc90edc8eb5d321bc0f8bd0589012bd6be71c
Boone B Gorges <boonebgorges@gmail.com>
2024-07-11 13:28:11 -0500

    Move openlab_docs_tabs() markup into a template file.
    See cuny-academic-commons/commons-in-a-box#473.

--- lib/menus.php
+++ lib/menus.php
@@ -1134,30 +1134,13 @@ function openlab_group_membership_tabs( $group = false ) {
 
 /**
  * Tabs for BuddyPress Docs navigation.
+ *
+ * @since 1.6.0 Markup was moved into theme template file.
+ *
+ * @return void
  */
 function openlab_docs_tabs() {
-	global $bp, $groups_template;
-
-	$group = null;
-	if ( bp_is_group() ) {
-		$group = groups_get_current_group();
-	} elseif ( ! empty( $groups_template->group ) ) {
-		$group = $groups_template->group;
-	}
-
-	$group_permalink = bp_get_group_permalink( $group );
-
-	?>
-
-	<li <?php echo ( 'list' === bp_docs_current_view() ? 'class="current-menu-item"' : '' ); ?> ><a href="<?php echo esc_url( $group_permalink . bp_docs_get_docs_slug() ); ?>/"><?php esc_html_e( 'View Docs', 'commons-in-a-box' ); ?></a></li>
-	<?php if ( current_user_can( 'bp_docs_create' ) && current_user_can( 'bp_docs_associate_with_group', bp_get_current_group_id() ) ) : ?>
-		<li <?php echo ( 'create' === bp_docs_current_view() ? 'class="current-menu-item"' : '' ); ?> ><a href="<?php echo esc_url( $group_permalink . bp_docs_get_docs_slug() ); ?>/create/"><?php esc_html_e( 'New Doc', 'commons-in-a-box' ); ?></a></li>
-	<?php endif; ?>
-	<?php if ( ( 'edit' === bp_docs_current_view() || 'single' === bp_docs_current_view() ) && bp_docs_is_existing_doc() ) : ?>
-		<?php $doc_obj = bp_docs_get_current_doc(); ?>
-		<li class="current-menu-item"><?php echo esc_html( $doc_obj->post_title ); ?></li>
-	<?php endif; ?>
-	<?php
+	bp_get_template_part( 'groups/single/nav/docs' );
 }
 
 function openlab_forum_tabs() {
