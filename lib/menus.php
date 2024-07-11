6ffffdd415e108ff6e3b8092425f6374c1f9fae3
Boone B Gorges <boonebgorges@gmail.com>
2024-07-11 13:34:20 -0500

    Move openlab_forum_tabs() markup into template file.
    Also fixes a bug that caused the incorrect topic title to show
    up in the Discussion subnav.
    
    See cuny-academic-commons/commons-in-a-box#473.

--- lib/menus.php
+++ lib/menus.php
@@ -1143,45 +1143,15 @@ function openlab_docs_tabs() {
 	bp_get_template_part( 'groups/single/nav/docs' );
 }
 
+/**
+ * Tabs for Forum navigation.
+ *
+ * @since 1.6.0 Markup was moved into theme template file.
+ *
+ * @return void
+ */
 function openlab_forum_tabs() {
-	global $bp, $groups_template, $wp_query;
-	$group = ( $groups_template->group ) ? $groups_template->group : $bp->groups->current_group;
-	// Load up bbPress once
-	$bbp = bbpress();
-
-	/** Query Resets ***************************************************** */
-	// Forum data
-	$forum_ids = bbp_get_group_forum_ids( bp_get_current_group_id() );
-	$forum_id  = array_shift( $forum_ids );
-	$offset    = 0;
-
-	$bbp->current_forum_id = $forum_id;
-
-	bbp_set_query_name( 'bbp_single_forum' );
-
-	// Get the topic
-	bbp_has_topics(
-		array(
-			'name'           => bp_action_variable( $offset + 1 ),
-			'posts_per_page' => 1,
-			'show_stickies'  => false,
-		)
-	);
-
-	// Setup the topic
-	while ( bbp_topics() ) {
-		bbp_the_topic();
-	}
-
-	$group_forum_permalink = bp_get_group_url( $group->id, bp_groups_get_path_chunks( [ 'forum' ] ) );
-	?>
-
-	<li <?php echo ( ! bp_action_variable() ? 'class="current-menu-item"' : '' ); ?> ><a href="<?php echo esc_attr( $group_forum_permalink ); ?>"><?php esc_html_e( 'Discussion', 'commons-in-a-box' ); ?></a></li><!--
-	<?php if ( bp_is_action_variable( 'topic' ) ) : ?>
-		--><li class="current-menu-item hyphenate"><span><?php bbp_topic_title(); ?></span></li><!--
-			<?php endif; ?>
-	-->
-	<?php
+	bp_get_template_part( 'groups/single/nav/forum' );
 }
 
 function openlab_is_create_group( $group_type ) {
--- lib/plugin-hooks.php
+++ lib/plugin-hooks.php
@@ -127,7 +127,7 @@ function openlab_forum_tabs_output() {
 	?>
 	<div class="forum-nav">
 		<ul class="nav nav-inline">
-			<?php openlab_forum_tabs(); ?>
+			<?php bp_get_template_part( 'groups/single/nav/forum' ); ?>
 		</ul>
 
 		<?php bbp_get_template_part( 'form', 'search' ); ?>
