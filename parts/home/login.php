bd348e813202a8693249d06347a4189327106d97
Boone B Gorges <boonebgorges@gmail.com>
2026-03-31 14:19:33 -0500

    More graceful fallback for homepage 'Need Help' text when brand pages aren't set up.
    See cuny-academic-commons/commons-in-a-box#542.

--- parts/home/login.php
+++ parts/home/login.php
@@ -36,6 +36,17 @@ if ( is_user_logged_in() ) :
 		$contact_link = $brand_pages['contact-us']['preview_url'];
 	}
 
+	if ( $help_link && $contact_link ) {
+		// translators: 1. help link, 2. contact link
+		$need_help_text = sprintf( 'Visit the <a class="roll-over-loss" href="%1$s">Help section</a> or <a class="roll-over-loss" href="%2$s">contact us</a> with a question.', esc_attr( $help_link ), esc_attr( $contact_link ) );
+	} elseif ( $help_link ) {
+		// translators: help link
+		$need_help_text = sprintf( 'Questions? Visit the <a class="roll-over-loss" href="%s">Help section</a>.', esc_attr( $help_link ) );
+	} elseif ( $contact_link ) {
+		// translators: contact link
+		$need_help_text = sprintf( '<a class="roll-over-loss" href="%s">Contact us</a> with questions.', esc_attr( $contact_link ) );
+	}
+
 	$user_avatar = bp_get_loggedin_user_avatar(
 		[
 			'type' => 'full',
@@ -72,8 +83,19 @@ if ( is_user_logged_in() ) :
 
 	<div id="login-help" class="log-box">
 		<h2 class="title"><?php esc_html_e( 'Need Help?', 'commons-in-a-box' ); ?></h2>
-		<?php /* translators: 1. help link, 2. contact link */ ?>
-		<p class="font-size font-14"><?php printf( 'Visit the <a class="roll-over-loss" href="%1$s">Help section</a> or <a class="roll-over-loss" href="%2$s">contact us</a> with a question.', esc_attr( $help_link ), esc_attr( $contact_link ) ); ?></p>
+		<p class="font-size font-14">
+			<?php
+			echo wp_kses(
+				$need_help_text,
+				[
+					'a' => [
+						'class' => true,
+						'href'  => true,
+					],
+				]
+			);
+			?>
+		</p>
 	</div><!--login-help-->
 
 <?php else : ?>
