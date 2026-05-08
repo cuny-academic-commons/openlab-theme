61911e472bc0a7109ddfbcb4c43ad36380f5eac6
Boone B Gorges <boonebgorges@gmail.com>
2026-05-08 12:27:04 -0500

    Use new method name when saving group AI crawler settings.
    See cuny-academic-commons/commons-in-a-box#560.

--- lib/group-funcs.php
+++ lib/group-funcs.php
@@ -1081,7 +1081,7 @@ function openlab_save_group_site_settings() {
 
 	if ( $site_id && isset( $_POST['cboxol_block_ai_robots'] ) ) {
 		$block_ai_robots = ! empty( $_POST['cboxol_block_ai_robots'] );
-		update_blog_option( $site_id, 'cboxol_block_ai_robots', $block_ai_robots ? 1 : 0 );
+		\CBOX\OL\Robots\set_block_ai_robots_enabled( $site_id, $block_ai_robots );
 	}
 
 	// Portfolio profile link
